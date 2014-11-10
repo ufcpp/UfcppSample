dc128f106a3005eabc32aa329161a915a35c7445
Nobuyuki Iwanaga <ufcpp@live.jp>
2014-11-11 00:10:04 +0900

    Query Expression のサンプル、Ver2でも通るものに差し替え

---
+++ LanguageAndFrameworkVersion/SharedProject/Csharp3/LinqToObjectSample.cs
@@ -0,0 +1,26 @@
+﻿#if Ver2Plus
+
+using System;
+using System.Linq;
+
+namespace VersionSample.Csharp3
+{
+    public class LinqToObjectSample
+    {
+        public void Run()
+        {
+            var input = new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var output =
+                from x in input
+                where (x % 2) == 1
+                select x * x;
+
+            foreach (var x in output)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
+
+#endif
--- LanguageAndFrameworkVersion/SharedProject/Csharp3/QueryExpressionSample.cs
+++ LanguageAndFrameworkVersion/SharedProject/Csharp3/QueryExpressionSample.cs
@@ -1,26 +1,53 @@
-﻿#if Ver2Plus
-
-using System;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace VersionSample.Csharp3
 {
     public class QueryExpressionSample
     {
-        public void Run()
+        public static void Run()
         {
-            var input = new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
             var output =
-                from x in input
-                where (x % 2) == 1
-                select x * x;
+                from x in new QueryLog<int>()
+                where true
+                select new QueryLog<string>();
 
-            foreach (var x in output)
-            {
-                Console.WriteLine(x);
-            }
+            var theSameAsAbove = new QueryLog<int>()
+                .Where(x => true)
+                .Select(x => new QueryLog<string>());
+
+            Console.WriteLine(output);
+        }
+    }
+
+    public delegate QueryLog<TResult> Selector<TSource, TResult>(QueryLog<TSource> item);
+    public delegate bool Predicate<TSource>(QueryLog<TSource> item);
+
+    public struct QueryLog<T>
+    {
+
+        private StringBuilder _log;
+
+        public QueryLog<TResult> Select<TResult>(Selector<T, TResult> selector)
+        {
+            _log = _log ?? new StringBuilder();
+            _log.AppendFormat(".Select<{0}, {1}>", typeof(T).Name, typeof(TResult).Name);
+            var x = selector(this);
+            x._log = _log;
+            return x;
+        }
+
+        public QueryLog<T> Where(Predicate<T> predicate)
+        {
+            _log = _log ?? new StringBuilder();
+            _log.AppendFormat(".Where<{0}>", typeof(T).Name);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return _log == null ? "" :  _log.ToString();
         }
     }
 }
-
-#endif
