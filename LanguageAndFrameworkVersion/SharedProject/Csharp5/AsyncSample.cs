7e747b88a1be76d80c9e0e2133fb584622ea3da9
Nobuyuki Iwanaga <ufcpp@live.jp>
2014-11-12 10:29:16 +0900

    Task.Delay

--- LanguageAndFrameworkVersion/SharedProject/Csharp5/AsyncSample.cs
+++ LanguageAndFrameworkVersion/SharedProject/Csharp5/AsyncSample.cs
@@ -29,4 +29,30 @@ namespace VersionSample.Csharp5
     }
 }
 
+namespace VersionSample.Csharp5
+{
+#if Ver4_5
+    using T = System.Threading.Tasks.Task;
+#else
+    using T = System.Threading.Tasks.TaskEx;
+#endif
+
+    /// <summary>
+    /// ただ、Microsoft.Bcl.Async では、.NET 4.5 との差分のメンバーを拡張メソッドとかで定義してるんだけども、
+    /// 静的メソッドの拡張はできないので、<see cref="Task.Delay(int)"/> みたいなやつは足せない。やむなく、TaskEx っていうクラスを作ってる。
+    /// このせいで、<see cref="Task.Delay(int)"/> とかを使うには、上記 #if みたいなマネが必要。
+    /// </summary>
+    public class AsyncTaskSample
+    {
+        public static async Task XAsync()
+        {
+            await T.Yield();
+            await T.Delay(1);
+            await T.Run(() => { });
+            await T.WhenAll(T.Delay(1), T.Run(() => { }));
+            await T.WhenAny(T.Delay(1), T.Run(() => { }));
+        }
+    }
+}
+
 #endif
