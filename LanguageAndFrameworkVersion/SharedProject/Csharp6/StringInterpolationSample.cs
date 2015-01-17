40df080fe18f1c965bd758481026a0d31bd6b6ae
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-01-18 03:49:31 +0900

    IFormattable string interpolation の話に追記
    CTP 5 でもまだみたいだけども、一応。

--- LanguageAndFrameworkVersion/SharedProject/Csharp6/StringInterpolationSample.cs
+++ LanguageAndFrameworkVersion/SharedProject/Csharp6/StringInterpolationSample.cs
@@ -32,12 +32,41 @@ namespace VersionSample.Csharp6
         }
 
         //【注意】この構文だとカルチャー指定とかができない(IFormatProviders を与えるオーバーロードを呼べない)。
-        // 次のプレビュー リリースで、
+        // 正式版までには、
         // IFormattable f = $"{x}, {y]";
-        // f.ToString(/* ここに provider を渡せる */);
+        // f.ToString(null, /* ここに provider を渡せる */);
         // みたいな追加の構文が入る予定。
         //
         // この機能には、System.Runtime.CompilerServices.FormattedString 型が必要になるので、.NET 4.6 (もしくは同等の FormattedString の自前実装)が必要に。
         // C# 6.0 の機能の中で、.NET 4.6/.NET Core 5を要求する唯一の機能。
+
+#if false//Ver4_5
+
+        public static void Y()
+        {
+            var x = 10;
+
+            IFormattable f = $"{x :c}, {x :n}";
+            var s = Culture(f, "en-us");
+
+            Console.WriteLine(s);
+        }
+
+        public static void SameAsY()
+        {
+            var x = 10;
+            var y = 20;
+
+            var s = string.Format("{0}, {1}", x, y);
+
+            Console.WriteLine(s);
+        }
+
+        private static string Culture(IFormattable f, string cultureName)
+        {
+            return f.ToString(null, System.Globalization.CultureInfo.CreateSpecificCulture(cultureName));
+        }
+
+#endif
     }
 }
