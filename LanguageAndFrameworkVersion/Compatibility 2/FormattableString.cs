96e810c85f51e410cc2800a3c7193c83ad2f2293
niwanaga <iwanaga@orange-cube.jp>
2015-02-27 10:12:59 +0900

    互換 FormattableString をちゃんと実�

--- LanguageAndFrameworkVersion/Compatibility 2/FormattableString.cs
+++ LanguageAndFrameworkVersion/Compatibility 2/FormattableString.cs
@@ -1,18 +1,52 @@
-﻿namespace System.Runtime.CompilerServices
-{
-    public class FormattableStringFactory
-    {
-        public static FormattableString Create(string format, params object[] args)
-        {
-            return null;
-        }
-    }
+﻿using System.Globalization;
 
-    public class FormattableString : IFormattable
+namespace System
+{
+    public abstract class FormattableString : IFormattable
     {
-        public string ToString(string format, IFormatProvider formatProvider)
+        public abstract string Format { get; }
+        public abstract int ArgumentCount { get; }
+        public abstract object[] GetArguments();
+        public abstract object GetArgument(int index);
+        public abstract string ToString(IFormatProvider formatProvider);
+        string IFormattable.ToString(string ignored, IFormatProvider formatProvider) => ToString(formatProvider);
+        public static string Invariant(FormattableString formattable)
         {
-            throw new NotImplementedException();
+            if (formattable == null)
+            {
+                throw new ArgumentNullException("formattable");
+            }
+            return formattable.ToString(CultureInfo.InvariantCulture);
+        }
+        public override string ToString() => ToString(CultureInfo.CurrentCulture);
+        protected FormattableString() { }
+    }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    public static class FormattableStringFactory
+    {
+        private sealed class ConcreteFormattableString : FormattableString
+        {
+            private readonly string _format;
+            private readonly object[] _arguments;
+            public override string Format => _format;
+            public override int ArgumentCount => _arguments.Length;
+            internal ConcreteFormattableString(string format, object[] arguments)
+            {
+                _format = format;
+                _arguments = arguments;
+            }
+            public override object[] GetArguments() => _arguments;
+            public override object GetArgument(int index) => _arguments[index];
+            public override string ToString(IFormatProvider formatProvider) => string.Format(formatProvider, _format, _arguments);
+        }
+        public static FormattableString Create(string format, params object[] arguments)
+        {
+            if (format == null) throw new ArgumentNullException("format");
+            if (arguments == null) throw new ArgumentNullException("arguments");
+            return new ConcreteFormattableString(format, arguments);
         }
     }
 }
