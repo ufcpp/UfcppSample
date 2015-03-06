1091feb593a916e771482fa22af8817cb4357a1a
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-03-06 23:53:50 +0900

    adds comment on EnumBaseTypeSystemInt32

--- BreakingChanges/VS2015_CS6/EnumBaseTypeSystemInt32.cs
+++ BreakingChanges/VS2015_CS6/EnumBaseTypeSystemInt32.cs
@@ -6,6 +6,10 @@
 /// <summary>
 /// Can be compiled in: C# 6
 ///
-/// enum base type can be a type expression like System.Int32
+/// Changed: enum base type can be a type expression like System.Int32
 /// </summary>
+/// <remarks>
+/// The ealier versions produce:
+/// error CS1008: Type byte, sbyte, short, ushort, int, uint, long, or ulong expected
+/// </remarks>
 class EnumBaseTypeSystemInt32 { static void Main() { } }
