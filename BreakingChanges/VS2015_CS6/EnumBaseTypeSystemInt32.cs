9376bd64263de199349f9df1d977a729f210d30b
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-03-06 23:50:35 +0900

    EnumBaseTypeSystemInt32

---
+++ BreakingChanges/VS2015_CS6/EnumBaseTypeSystemInt32.cs
@@ -0,0 +1,11 @@
+﻿enum X : System.Int32
+{
+    A, B, C,
+}
+
+/// <summary>
+/// Can be compiled in: C# 6
+///
+/// enum base type can be a type expression like System.Int32
+/// </summary>
+class EnumBaseTypeSystemInt32 { static void Main() { } }
