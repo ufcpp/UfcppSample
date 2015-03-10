e395a41037c383ef872b70feef8bae893a9ffd5d
niwanaga <iwanaga@orange-cube.jp>
2015-03-10 10:08:35 +0900

    StaticFieldOfAnotherInstantiation

---
+++ BreakingChanges/VS2015_CS6/CyclicalConstructorChaining.cs
@@ -0,0 +1,16 @@
+﻿class C
+{
+    public C(int x) : this() { }
+    public C() : this(0) { }
+}
+
+// In C# it is a compile-time error to write a constructor that recursively chains directly to itself
+//class C1
+//{
+//    public C(int x) : this(x + 1) { }
+//}
+
+class CyclicalConstructorChaining
+{
+    static void Main() { }
+}
