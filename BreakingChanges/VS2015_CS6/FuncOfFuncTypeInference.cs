7f4a15fd9bfdda92dceecaf33ad2f99f4ce30e92
niwanaga <iwanaga@orange-cube.jp>
2015-03-16 10:41:49 +0900

    仮

---
+++ BreakingChanges/VS2015_CS6/FuncOfFuncTypeInference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class FuncOfFuncTypeInference
+{
+    static void Main()
+    {
+        X(() => 10);
+        X(() => () => 10);
+    }
+
+    private static T X<T>(Func<Func<T>> f) { return f()(); }
+    private static T X<T>(Func<T> f) { return f(); }
+}
