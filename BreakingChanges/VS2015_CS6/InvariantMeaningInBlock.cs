a28dab08c7ffa8b2cc9f26c873d4770e021a16f5
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-04-25 14:03:29 +0900

    "invariant meaning" rule

---
+++ BreakingChanges/VS2015_CS6/InvariantMeaningInBlock.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// Can be compiled in: C# 6
+///
+/// https://github.com/dotnet/roslyn/issues/2110
+///
+/// The C# specification had "invariant meaning" rule: https://msdn.microsoft.com/en-us/library/aa691351(v=vs.71).aspx
+/// Roslyn has gotten rid of the "invariant meaning" rule.
+/// </summary>
+class InvariantMeaningInBlock
+{
+    double x;
+
+    void F(bool b)
+    {
+        x = 1.0;
+        if (b)
+        {
+            int x;
+            x = 1;
+        }
+    }
+
+    static void Main()
+    {
+        var x = new InvariantMeaningInBlock();
+        x.F(true);
+        x.F(false);
+    }
+}
+
