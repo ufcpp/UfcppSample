b62340ac961ac24b35764a4ebd52d4afd1d661a2
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-05-12 22:58:17 +0900

    devenv

---
+++ DevEnv/ConsoleApplication1/ConsoleApplication1/Logic.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApplication1
+{
+    class Logic
+    {
+        public int Filter(int x) => x * x;
+    }
+}
---
+++ DevEnv/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Logic();
+            Console.WriteLine(x.Filter(10));
+        }
+    }
+}
