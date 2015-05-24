f276590a45369a0caccee244cd4c59bafec910fc
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-05-25 00:15:39 +0900

    C# 6.0細かく修正
    5.0との比較になってないコードを幾つか、miscに追加
    構造体の引数なしコンストラクター、仕様から消えたのでサンプルコード削除

---
+++ Csharp6/Csharp6/Misc/StringInterpolationSample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp6.Misc
+{
+    class StringInterpolationSample
+    {
+        public void X()
+        {
+            var x = 12300;
+
+            // ほぼ同じ意味
+            Console.WriteLine(string.Format("{0,4:x}", x));
+            Console.WriteLine($"{x,4:x}");
+
+            // 書き方を忘れて、 , と : を間違えてしまうと…
+
+            // 実行時エラー
+            Console.WriteLine(string.Format("{0,x}", x));
+
+            // コンパイル エラー
+            //Console.WriteLine($"{x,x}");
+
+            // エスケープ
+            var p = new { X = 10, Y = 20 };
+            Console.WriteLine($"\"{{{p.X}, {p.Y}}}\"");
+
+            // こちらは、逐語的文字列リテラルと同じルール
+            var verbatim = $@"
+""
+{{
+{p.X}\{p.Y}
+}}
+""
+";
+        }
+    }
+}
