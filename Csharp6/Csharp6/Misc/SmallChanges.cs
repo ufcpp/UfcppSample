bb6000d463e0ecf83383fc83b50a685afc0e47a3
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-05-24 16:34:47 +0900

    プロパティ初期化

---
+++ Csharp6/Csharp6/Misc/SmallChanges.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp6.Misc
+{
+    class SmallChanges
+    {
+    }
+
+    struct Point
+    {
+        public int X { get; private set; }
+
+        public Point(int x)
+        {
+            // C# 5.0まではエラーに。
+            X = x;
+        }
+    }
+}
