0350583ba926fe123a4397a55e26e514835d9f7b
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-05-31 23:50:08 +0900

    CompilerPlatformデモ nameof

---
+++ Demo/2015/CompilerPlatform/NameofDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WriteLine(nameof(Person.Key) == "Id");
+    }
+}
+
+class Person
+{
+    public string Key { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
