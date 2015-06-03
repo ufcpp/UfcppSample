4bec4999e822a92dfecd9edaaf89270240272d2d
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-06-04 00:10:59 +0900

    CompilerPlatform デモ FluentArithmetic

---
+++ Demo/2015/CompilerPlatform/FluentArithmeticDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentArithmetic;
+
+namespace FluentArithmeticDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 1
+                .Add(2)
+                .Mul(3)
+                .Sub(1)
+                .Div(4);
+
+            Console.WriteLine(x);
+        }
+    }
+}
