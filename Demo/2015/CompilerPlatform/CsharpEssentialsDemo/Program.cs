ca93f4a3385da1c0a302ce810fcbf652003641c8
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-06-06 23:39:07 +0900

    CompilerPlatform デモ C# Essentials

---
+++ Demo/2015/CompilerPlatform/CsharpEssentialsDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CsharpEssentialsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Point
+    {
+        public int X { get; private set; }
+
+        public int Y { get { return _y; } }
+        private int _y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            _y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
