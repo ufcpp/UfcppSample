fa17e67b1a73b05bd44d4897e205219c5ecb21ac
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-06-06 18:51:44 +0900

    CompilerPlatform デモ Record

---
+++ Demo/2015/CompilerPlatform/RecordDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RecordDemo
+{
+    class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
