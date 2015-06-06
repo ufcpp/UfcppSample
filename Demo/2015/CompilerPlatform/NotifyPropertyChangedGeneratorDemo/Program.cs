b8253dee1412c690ce4ab5802d8d834f403edff6
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-06-06 23:54:43 +0900

    CompilerPlatform デモ NPCGenerator

---
+++ Demo/2015/CompilerPlatform/NotifyPropertyChangedGeneratorDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace NotifyPropertyChangedGeneratorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    [Notify]
+    public class Sample1
+    {
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    [Notify(NotifyCompareMethod.None)]
+    public class Sample2
+    {
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
