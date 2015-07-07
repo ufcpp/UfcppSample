df632d93d9b9a12a98c7c6a7b08a78d24072676c
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-07-07 21:17:48 +0900

    using static enum

---
+++ Csharp6/Csharp6/Misc/UsingStaticEnum.cs
@@ -0,0 +1,20 @@
+﻿using static Color;
+
+class UsingStaticEnum
+{
+    public void X()
+    {
+        // enum のメンバーも using static で参照できる
+        var cyan = Blue | Green;
+        var purple = Red | Blue;
+        var yellow = Red | Green;
+    }
+}
+
+enum Color
+{
+    Red = 1,
+    Green = 2,
+    Blue = 4,
+}
+
