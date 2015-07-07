2cd6399f311efe22d7a8d75b822604f876ff5066
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-07-07 21:14:19 +0900

    using static 関連サンプル2つ追加

---
+++ Csharp6/Csharp6/Misc/UsingStaticExtensions.cs
@@ -0,0 +1,19 @@
+﻿using static System.Linq.Enumerable;
+
+class UsingStaticExtensions
+{
+    public void X()
+    {
+        // 普通の静的メソッド
+        // Enumerable.Range が呼ばれる
+        var input = Range(0, 10);
+
+        // 拡張メソッド
+        // Enumerable.Select が呼ばれる
+        var output1 = input.Select(x => x * x);
+
+        // 拡張メソッドを普通の静的メソッドとして呼ぼうとすると
+        // コンパイル エラー
+        //var output2 = Select(input, x => x * x);
+    }
+}
---
+++ Csharp6/Csharp6/Misc/UsingStaticNormalClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using static System.Threading.Tasks.Task;
+using static System.TimeSpan;
+
+class UsingStaticNormalClass
+{
+    public async Task XAsync()
+    {
+        // TimeSpan.FromSeconds
+        var sec = FromSeconds(1);
+
+        // Task.Delay
+        await Delay(sec);
+    }
+}
