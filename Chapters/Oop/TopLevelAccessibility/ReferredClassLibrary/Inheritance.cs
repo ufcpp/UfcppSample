44c161fb37f672fc28f4b5dfeb620deb1fc12e6e
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-07-14 22:52:34 +0900

    追記 & fig

---
+++ Chapters/Oop/TopLevelAccessibility/ReferredClassLibrary/Inheritance.cs
@@ -0,0 +1,24 @@
+﻿namespace B
+{
+    public struct PublicStruct { }
+    public interface PublicInterface { PublicStruct X { get; } }
+
+    internal struct InternalStruct { }
+    internal interface InternalInterface { InternalStruct X { get; } }
+
+    public class PublicClass : PublicInterface, InternalInterface
+    {
+        public PublicStruct X => default(PublicStruct);
+        InternalStruct InternalInterface.X => default(InternalStruct);
+    }
+}
+
+namespace C
+{
+    internal class InternalClass { }
+
+    // コンパイル エラー
+    public class PublicClass : InternalClass
+    {
+    }
+}
---
+++ Chapters/Oop/TopLevelAccessibility/ReferredClassLibrary/Nested.cs
@@ -0,0 +1,8 @@
+﻿public class TopLevelClass
+{
+    public class PublicClass { }
+    protected class ProtectedClass { }
+    protected internal class ProtectedInternalClass { }
+    internal class InternalClass { }
+    private class PrivateClass { }
+}
