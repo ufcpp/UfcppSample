ded15bcc0effa8fe53193e45a9a02da55490221d
niwanaga <iwanaga@orange-cube.jp>
2015-07-28 12:31:14 +0900

    MigrateToProject.json

---
+++ Scribble/MigrateToProjectJson/Package.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace MigrateToProjectJson
+{
+    class Package
+    {
+        public string Id { get; set; }
+        public string Version { get; set; }
+
+        public string AnyVersion => string.Join(".", Version.Split('.').Take(2)) + ".*";
+
+        public override string ToString() => $"{Id}: {Version}";
+    }
+}
