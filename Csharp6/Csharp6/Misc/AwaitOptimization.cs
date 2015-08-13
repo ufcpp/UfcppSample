9428bf0e31fba9d05631afe1929246f0154e4815
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-08-13 19:38:36 +0900

    await optimization

---
+++ Csharp6/Csharp6/Misc/AwaitOptimization.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp6.Misc
+{
+    class AwaitOptimization
+    {
+        public static IEnumerable<int> GetXItems()
+        {
+            var x = 10;
+            yield return x;
+
+            var y = x * x;  // x は yield を超えて使っている
+            yield return y; // y は yield を超えない
+
+            yield return x;
+        }
+
+        public static async Task XAsync()
+        {
+            var x = 10;
+            await Task.Delay(x);
+
+            var y = x * x;       // x は await を超えて使っている
+            await Task.Delay(y); // y は await を超えない
+
+            await Task.Delay(x);
+        }
+
+    }
+}
