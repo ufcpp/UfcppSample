7c188d59acae345aff89a2f19a19c4c5581493de
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-01-15 22:27:00 +0900

    C# Scripting サンプル

---
+++ Chapters/Scripting/src/Scripting/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MainAsync().Wait();
+    }
+
+    private static async Task MainAsync()
+    {
+        var result = await CSharpScript.EvaluateAsync<int>(@"
+var x = 1;
+var y = 2;
+x + y
+");
+        Console.WriteLine(result);
+    }
+}
