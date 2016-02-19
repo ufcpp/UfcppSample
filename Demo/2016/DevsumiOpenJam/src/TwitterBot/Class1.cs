58f9b322ba86f76aed4373ecf80aa4cc81ceff24
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-02-19 17:03:18 +0900

    デブサミ2016 オープンジャム用 twitter ボット

---
+++ Demo/2016/DevsumiOpenJam/src/TwitterBot/Class1.cs
@@ -0,0 +1,31 @@
+﻿using CoreTweet;
+using System;
+using System.Threading.Tasks;
+
+namespace TwitterBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            var keys = new
+            {
+                ConsumerKey = "[you cosumer key]",
+                ConsumerSecret = "[you consumer secret",
+                AccessToken = "[access token to a bot account]",
+                AccessSecret = "[access secret to a bot account]",
+            };
+
+            var tokens = Tokens.Create(keys.ConsumerKey, keys.ConsumerSecret, keys.AccessToken, keys.AccessSecret);
+            var message = $@".NET Core からつぶやき
+{string.Join(" ", args)}
+{DateTime.Now}";
+            await tokens.Statuses.UpdateAsync(new { status = message });
+        }
+    }
+}
