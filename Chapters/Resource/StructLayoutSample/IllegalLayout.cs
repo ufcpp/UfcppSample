831d8a12a141cc24b3cf05fbac27641f2f615180
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-06-19 00:32:22 +0900

    Explicitレイアウト、値と参照を重ねると実行時エラー

---
+++ Chapters/Resource/StructLayoutSample/IllegalLayout.cs
@@ -0,0 +1,25 @@
+﻿namespace StructLayoutSample.IllegalLayout
+{
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Explicit)]
+    struct Sample
+    {
+        [FieldOffset(0)]
+        public int A;
+
+        // 値と参照を同じ場所にレイアウト
+        // コンパイル エラーにはならない
+        [FieldOffset(0)]
+        public object B;
+    }
+
+    class Program
+    {
+        static unsafe void Main()
+        {
+            // Sample 型に触れた瞬間、実行時エラーになる
+            var s = new Sample();
+        }
+    }
+}
