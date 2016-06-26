8ab93399de38676c96f856add99e7ee3787beef4
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-06-26 12:23:08 +0900

    .NET Coreライブラリ追加

---
+++ Demo/2016/NetStandard/ClassLibraryNetCore/Class1.cs
@@ -0,0 +1,16 @@
+﻿using FluentArithmetic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassLibraryNetCore
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            var x = 1.Add(2);
+        }
+    }
+}
