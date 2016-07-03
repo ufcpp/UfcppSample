6f3b18fa77c4fa081db4e59b323775997c922191
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-07-03 15:29:44 +0900

    標準ベースライブラリから既存の非対応ライブラリを参照するデモ

---
+++ Demo/2016/NetStandard/ClassLibraryImports/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibraryImports
+{
+    public class Class1
+    {
+        void X()
+        {
+            var x = Newtonsoft.Json.JsonConvert.DeserializeObject("{ \"a\": 1 }");
+        }
+    }
+}
