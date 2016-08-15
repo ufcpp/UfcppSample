08f16390f2f55e459d26c064bb374f2a3058a08e
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-08-15 20:57:52 +0900

    ポインター演算の例

---
+++ Chapters/Interop/Unsafe/Unsafe/PointerOperators.cs
@@ -0,0 +1,41 @@
+﻿namespace Unsafe.PointerOperators
+{
+    using System;
+
+    struct Point
+    {
+        public short X;
+        public short Y;
+    }
+
+    class Program
+    {
+        unsafe static void Main()
+        {
+            var p = new Point();
+
+            // アンマネージ型の変数にはポインターを使える
+            // & でアドレス取得(ポインター化)
+            // 型推論(var)も効く
+            var pp = &p;
+
+            // int 型のポインターに無理やり代入
+            // p のある位置に無理やり int の値を書き込み
+            int* pi = (int*)pp;
+            *pi = 0x00010002;
+
+            // -> で構造体のポインターのメンバーにアクセス
+            Console.WriteLine(pp->X); // (*pp).X と同じ意味 = 2
+            Console.WriteLine(pp->Y); // (*pp).Y と同じ意味 = 1
+
+            // byte 型のポインターに無理やり代入
+            byte* pb = (byte*)pp;
+
+            // ポインターには配列と同じように [] が使える
+            Console.WriteLine(pb[0]); // *(pb + 0) と同じ意味 = 2
+            Console.WriteLine(pb[1]); // *(pb + 1) と同じ意味 = 0
+            Console.WriteLine(pb[2]); // *(pb + 2) と同じ意味 = 1
+            Console.WriteLine(pb[3]); // *(pb + 3) と同じ意味 = 0
+        }
+    }
+}
--- Chapters/Interop/Unsafe/Unsafe/Program.cs
+++ Chapters/Interop/Unsafe/Unsafe/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Unsafe
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            unsafe
+            {
+                int n;
+                int* pn = &n;        // n のアドレスをポインター pn に代入。
+                byte* p = (byte*)pn; // 違う型のポインターに無理やり代入可能。
+
+                *p = 0x78; // n の最初の1バイト目に 0x78 を代入
+                ++p;
+                *p = 0x56; // n の2バイト目に 0x56 を代入
+                ++p;
+                *p = 0x34; // n の3バイト目に 0x34 を代入
+                ++p;
+                *p = 0x12; // n の4バイト目に 0x12 を代入
+
+                Console.Write("{0:x}\n", n); // n の値を16進数で表示。
+            }
         }
     }
 }
