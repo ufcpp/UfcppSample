262d00b905494988955e012a29a020264251ebe4
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-08-16 23:22:19 +0900

    タプル

---
+++ Chapters/StructuredProgramming/Tuples/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static (int min, int max, double average) Measure(IEnumerable<int> items)
+        {
+            var count = 0;
+            var sum = 0;
+            var min = int.MaxValue;
+            var max = int.MinValue;
+            foreach (var x in items)
+            {
+                sum += x;
+                count++;
+                min = Math.Min(x, min);
+                max = Math.Max(x, max);
+            }
+
+            return (min, max, (double)sum / count);
+        }
+    }
+}
---
+++ Chapters/StructuredProgramming/Tuples/X/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tuples.X
+{
+    class Program
+    {
+        static X Measure(IEnumerable<int> items)
+        {
+            var count = 0;
+            var sum = 0;
+            var min = int.MaxValue;
+            var max = int.MinValue;
+            foreach (var x in items)
+            {
+                sum += x;
+                count++;
+                min = Math.Min(x, min);
+                max = Math.Max(x, max);
+            }
+
+            return new X(min, max, (double)sum / count);
+        }
+    }
+
+    internal struct X
+    {
+        public int max;
+        public int min;
+        public double v;
+
+        public X(int min, int max, double v)
+        {
+            this.min = min;
+            this.max = max;
+            this.v = v;
+        }
+    }
+}
