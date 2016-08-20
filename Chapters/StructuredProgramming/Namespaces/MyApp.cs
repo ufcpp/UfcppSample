0a4d3fb7ecbf4492def6634410b29744309adf5f
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-08-20 13:17:53 +0900

    名前空間の解決優先度

---
+++ Chapters/StructuredProgramming/Namespaces/MyApp.cs
@@ -0,0 +1,46 @@
+﻿using static System.Console;
+using A;
+
+// using よりは、直接定義されているものの方が優先 A < C, global
+// エイリアスと型定義は同列 C = global
+using Lib = C.Lib;
+class Lib { public static void F() => WriteLine("global"); }
+
+namespace MyApp
+{
+    using B; // 内側に using を書くと、外より優先 A, C, global < B
+
+    // 同一名前空間内にあるものは1番高い優先度 B < MyApp
+    class Lib { public static void F() => WriteLine("MyApp"); }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Lib は5つある
+            // この場合 MyApp.Lib が使われる
+            // 優先度 高 MyApp > B > global = C > A 低
+            Lib.F();
+
+            // ちゃんと呼び分けたければフルネームで書く
+            A.Lib.F();
+            B.Lib.F();
+            C.Lib.F();
+            MyApp.Lib.F();
+            global::Lib.F();
+        }
+    }
+}
+
+namespace A
+{
+    class Lib { public static void F() => WriteLine("A"); }
+}
+namespace B
+{
+    class Lib { public static void F() => WriteLine("B"); }
+}
+namespace C
+{
+    class Lib { public static void F() => WriteLine("C"); }
+}
---
+++ Chapters/StructuredProgramming/Namespaces/global.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace X.Y
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 単に Y って書くと、名前空間 X.Y の方の意味になる
+            global::Y.F();
+        }
+    }
+}
+
+class Y { public static void F() => WriteLine("class B"); }
