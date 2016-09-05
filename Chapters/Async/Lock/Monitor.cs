78939e18e9354f7cada841563e26a815902d1df6
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-09-05 18:39:59 +0900

    lock

---
+++ Chapters/Async/Lock/Monitor.cs
@@ -0,0 +1,46 @@
+﻿namespace Lock.Monitor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main()
+        {
+            const int ThreadNum = 20;
+            const int LoopNum = 20;
+            var num = 0;
+
+            var syncObject = new object();
+
+            Parallel.For(0, ThreadNum, i =>
+            {
+                for (int j = 0; j < LoopNum; j++)
+                {
+                    bool lockTaken = false;
+                    try
+                    {
+                        Monitor.TryEnter(syncObject, ref lockTaken); // ロック取得
+
+                        //↓クリティカルセクション
+                        int tmp = num;
+                        Thread.Sleep(1);
+                        num = tmp + 1;
+                        //↑クリティカルセクション
+                    }
+                    finally
+                    {
+                        if (lockTaken)
+                            Monitor.Exit(syncObject); // ロック解放
+                    }
+                }
+            });
+
+            Console.WriteLine(num);
+        }
+    }
+}
