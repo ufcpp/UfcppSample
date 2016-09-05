be6cf7a434ceb68a46916dd760b18dd89ecd62fc
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-09-05 18:16:17 +0900

    out var

---
+++ Chapters/Data/OutVar/Point.cs
@@ -0,0 +1,36 @@
+﻿namespace OutVar.Point
+{
+    using System;
+
+    struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void GetCoordinate(out int x, out int y)
+        {
+            x = X;
+            y = Y;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // x, y のスコープはこのブロック内
+            // この辺りで x, y という名前の変数は作れない
+
+            var p = new Point { X = 1, Y = 2 };
+            p.GetCoordinate(out var x, out var y);
+
+            // 以下のような書き方をしたのと同じ
+            // int x, y;
+            // p.GetCoordinate(out x, out y);
+
+            // この行から下で x, y を使える
+
+            Console.WriteLine($"{x}, {y}");
+        }
+    }
+}
---
+++ Chapters/Data/OutVar/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace OutVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        class csharp6
+        {
+            static int? ParseOrDefault(string s)
+            {
+                int x;
+                return int.TryParse(s, out x) ? x : default(int?);
+            }
+        }
+
+        class csharp7int
+        {
+            static int? ParseOrDefault(string s)
+            {
+                return int.TryParse(s, out int x) ? x : default(int?);
+            }
+        }
+
+        class csharp7var
+        {
+            static int? ParseOrDefault(string s)
+            {
+                return int.TryParse(s, out var x) ? x : default(int?);
+            }
+        }
+
+        class csharp7expression
+        {
+            static int? ParseOrDefault(string s) => int.TryParse(s, out var x) ? x : default(int?);
+        }
+    }
+}
