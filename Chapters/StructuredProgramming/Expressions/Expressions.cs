2c3164d15e670200f1b360eac0d0804968e6d4e6
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-09-27 02:02:48 +0900

    式とステートメント fig

---
+++ Chapters/StructuredProgramming/Expressions/Expressions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Expressions
+{
+    class Expressions
+    {
+        int _x = 10 * 1000 * 1000;
+
+        Expressions() : this(2 * 3) { }
+        Expressions(int n) { }
+
+        static void M()
+        {
+            var x = 1 + 2 * 3 > 4 || 5 * 6 + 8 < 9;
+            var y = Math.Max(1 + (2 + 3) + 4, 5 + 6);
+        }
+    }
+}
---
+++ Chapters/StructuredProgramming/Expressions/Statements.cs
@@ -0,0 +1,23 @@
+﻿namespace Expressions
+{
+    class Statements
+    {
+        static void M()
+        {
+            var x = 10;
+
+            if (x > 5)
+            {
+            }
+            else
+            {
+            }
+
+            int[] items = { 1, 2, 3 };
+
+            foreach (var item in items)
+            {
+            }
+        }
+    }
+}
