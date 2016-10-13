e85efa439e633074288fff1789f2a485d8ba8b8a
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-10-13 10:46:49 +0900

    テスト整備

---
+++ Demo/2016/Unicode/UnitTestUtfString/CharacterInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Text.Encoding;
+
+namespace UnitTestUtfString
+{
+    struct CharacterInfo
+    {
+        public int codePoint;
+        public string @char;
+        public byte[] utf8;
+        public byte[] utf16;
+        public byte[] utf32;
+
+        public static IEnumerable<CharacterInfo> GetCharacters(string s)
+        {
+            var bytes = UTF32.GetBytes(s);
+
+            for (int i = 0; i < bytes.Length; i += 4)
+            {
+                var cp = new CharacterInfo();
+
+                var utf32 = new byte[4];
+                Array.Copy(bytes, i, utf32, 0, 4);
+                cp.utf32 = utf32;
+
+                cp.codePoint = (utf32[0] << 0)
+                    | (utf32[1] << 8)
+                    | (utf32[2] << 16)
+                    | (utf32[3] << 24);
+
+                var decoded = UTF32.GetString(utf32);
+                cp.@char = decoded;
+                cp.utf16 = Unicode.GetBytes(decoded);
+                cp.utf8 = UTF8.GetBytes(decoded);
+
+                yield return cp;
+            }
+        }
+    }
+}
---
+++ Demo/2016/Unicode/UnitTestUtfString/TestString.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utf8String = UtfString.Utf8.String;
+using Utf16String = UtfString.Utf16.String;
+using static System.Text.Encoding;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestUtfString
+{
+    [TestClass]
+    public class TestString
+    {
+        private static readonly IEnumerable<string> testData = new[]
+        {
+            "",
+            "aáαℵあáあ゙亜👩👩🏽",
+            "aαあ😀",
+            "아조선글",
+            "👨‍👨‍👨‍👨‍👨‍👨‍👨",
+            "👨‍👩‍👦‍👦",
+            "👨🏻‍👩🏿‍👦🏽‍👦🏼",
+            "́",
+            "♢♠♤",
+            "🀄♔",
+            "☀☂☁",
+            "∀∂∋",
+            "ᚠᛃᚻ",
+            "𩸽",
+        };
+
+        [TestMethod]
+        public void ShouldBeIdentical()
+        {
+            foreach (var item in testData)
+            {
+                ShouldBeIdentical(item);
+            }
+        }
+
+        private void ShouldBeIdentical(String s)
+        {
+            var utf8 = new Utf8String(UTF8.GetBytes(s));
+            var cp8 = utf8.ToArray();
+
+            var utf16 = new Utf16String(Unicode.GetBytes(s));
+            var cp16 = utf16.ToArray();
+
+            var info = CharacterInfo.GetCharacters(s).ToArray();
+
+            Assert.AreEqual(info.Length, cp8.Length);
+            Assert.AreEqual(info.Length, cp16.Length);
+
+            for (int i = 0; i < info.Length; i++)
+            {
+                Assert.AreEqual((uint)info[i].codePoint, cp8[i].Value);
+                Assert.AreEqual((uint)info[i].codePoint, cp16[i].Value);
+            }
+        }
+
+        [TestMethod]
+        public void NoAllocationWithForeach()
+        {
+            foreach (var item in testData)
+            {
+                NoAllocationWithForeach(item);
+            }
+        }
+
+        private void NoAllocationWithForeach(string s)
+        {
+            const int N = 1000;
+            var utf8 = new Utf8String(UTF8.GetBytes(s));
+
+            {
+                var start = GC.GetTotalMemory(false);
+
+                for (int i = 0; i < N; i++)
+                    foreach (var x in utf8)
+                        ;
+
+                var end = GC.GetTotalMemory(false);
+
+                Assert.AreEqual(start, end);
+            }
+
+            var utf16 = new Utf16String(Unicode.GetBytes(s));
+
+            {
+                var start = GC.GetTotalMemory(false);
+
+                for (int i = 0; i < N; i++)
+                    foreach (var x in utf16)
+                        ;
+
+                var end = GC.GetTotalMemory(false);
+
+                Assert.AreEqual(start, end);
+            }
+        }
+    }
+}
