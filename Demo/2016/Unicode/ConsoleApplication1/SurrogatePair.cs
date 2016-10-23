61d91142e2bd303f36ec9e4047def5b3f9314f0b
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-10-23 15:29:39 +0900

    SurrogatePair

---
+++ Demo/2016/Unicode/ConsoleApplication1/SurrogatePair.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class SurrogatePair
+    {
+        public static void Run()
+        {
+            var s = "𩸽";
+
+            // 「𩸽」はサロゲート ペアになっているので、char としては2文字になる
+            foreach (var c in s)
+            {
+                // Surrogate が2回表示される
+                Console.WriteLine(char.GetUnicodeCategory(c));
+            }
+
+            // string を受け取る GetUnicodeCategory オーバーロードなら、
+            // サロゲート ペアな文字もコード ポイントにデコードして正しくカテゴリー判定できる
+
+            // OtherLetter (漢字のカテゴリー)が表示される
+            Console.WriteLine(char.GetUnicodeCategory(s, 0));
+        }
+    }
+}
