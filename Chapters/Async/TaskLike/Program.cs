445faa0e0297d8554904ce5c8a2e8909fd56ac90
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-10-25 23:09:22 +0900

    Task-like のサンプル追加

---
+++ Chapters/Async/TaskLike/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TaskLike
+{
+    using System;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static async ValueTask<int> XAsync(Random r)
+        {
+            if (r.NextDouble() < 0.99)
+            {
+                // 99% ここを通る。
+                // この場合、await が1度もなく、非同期処理にならない。
+                // 非同期処理じゃないのに Task<int> のインスタンスが作られるのはもったいない
+                return 1;
+            }
+
+            // こちら側は本当に非同期処理なので、Task<int> が必要。
+            await Task.Delay(100);
+            return 0;
+        }
+
+        static Task<int> _cache;
+
+        // キャッシュしてるものなので、少し時間がたてば、確実に完了済みになる。
+        static Task<int> CachedX => _cache ?? (_cache = Task.Run(() => 1));
+
+        // 完了済みだと非同期処理にならない。
+        // 非同期処理じゃないのに Task<int> のインスタンスが作られるのはもったいない
+        static async ValueTask<int> Y() => await CachedX;
+        static async ValueTask<int> Z() => await Y();
+    }
+}
---
+++ Chapters/Async/TaskLike/TaskLike.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[AsyncMethodBuilder(typeof(AsyncValueTaskMethodBuilder<>))]
+struct TaskLike<TResult>
+{
+}
+
+struct AsyncValueTaskMethodBuilder<TResult>
+{
+    public static AsyncValueTaskMethodBuilder<TResult> Create() => default(AsyncValueTaskMethodBuilder<TResult>);
+    public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine { }
+    public void SetStateMachine(IAsyncStateMachine stateMachine) { }
+    public void SetResult(TResult result) { }
+    public void SetException(Exception exception) { }
+    public TaskLike<TResult> Task { get; }
+    public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+        where TAwaiter : INotifyCompletion
+        where TStateMachine : IAsyncStateMachine
+    { }
+    public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+        where TAwaiter : ICriticalNotifyCompletion
+        where TStateMachine : IAsyncStateMachine
+    { }
+}
+
+#if false
+namespace System.Runtime.CompilerServices
+{
+    sealed class AsyncMethodBuilderAttribute : Attribute
+    {
+        public AsyncMethodBuilderAttribute(Type builderType)
+        {
+            BuilderType = builderType;
+        }
+
+        public Type BuilderType { get; }
+    }
+}
+#endif
