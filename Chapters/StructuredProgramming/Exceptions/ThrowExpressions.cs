4ebe29466d31f6e7e34deb448ba4c2478113826f
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-10-25 23:09:55 +0900

    throw式のサンプル追加

---
+++ Chapters/StructuredProgramming/Exceptions/ThrowExpressions.cs
@@ -0,0 +1,36 @@
+﻿namespace Exceptions.ThrowExpressions
+{
+    using System;
+
+    class Program
+    {
+        // 式形式のメソッドの中( => の直後)
+        static void A() => throw new NotImplementedException();
+
+        static string B(object obj)
+        {
+            // null 合体演算子(??)の後ろ
+            var s = obj as string ?? throw new ArgumentException(nameof(obj));
+
+            // 条件演算子(?:)の条件以外の部分
+            return s.Length == 0 ? "empty" :
+                s.Length < 5 ? "short" :
+                throw new InvalidOperationException("too long");
+        }
+
+        static void C()
+        {
+            // コンパイル エラー。この文脈に throw 式は書けない
+            B(throw new InvalidOperationException());
+        }
+
+        static void D()
+        {
+            // コンパイル エラー。null(型を持っていない)と並べると型が決まらない。
+            var x = true ? null : throw new Exception();
+
+            // コンパイル エラー。throw 式同士を並べると型が決まらない。
+            var y = true ? throw new InvalidOperationException() : throw new NotSupportedException();
+        }
+    }
+}
---
+++ Chapters/StructuredProgramming/Function/ExpressionBodied.cs
@@ -0,0 +1,53 @@
+﻿namespace Function.ExpressionBodied
+{
+    using System;
+
+    // C# 6 時点で => を使って書ける関数メンバー
+    class Csharp6
+    {
+        // メソッド
+        int Method(int x) => x * x;
+
+        // 演算子
+        public static Csharp6 operator +(Csharp6 x) => x;
+
+        // プロパティ(get-only)
+        int X => 0;
+
+        // インデクサー(get-only)
+        int this[int index] => index;
+    }
+
+    // C# 7 で追加された => を使って書ける関数メンバー
+    class Csharp7
+    {
+        static int x;
+
+        // コンストラクター
+        Csharp7() => x++;
+
+        // デストラクター
+        ~Csharp7() => x--;
+
+        // プロパティ(get/set)
+        int X
+        {
+            get => x++;
+            set => x--;
+        }
+
+        // インデクサー(get/set)
+        int this[int index]
+        {
+            get => x += index;
+            set => x -= index;
+        }
+
+        // イベント(add/remove)
+        event Action E
+        {
+            add => x++;
+            remove => x--;
+        }
+    }
+}
