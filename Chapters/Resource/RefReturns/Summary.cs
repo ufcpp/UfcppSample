fc7e492b3d009f78d1d8907a2787ab8348ed2bcd
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-10-30 12:23:00 +0900

    ref returnとかliteralとかのサマリー追加

---
+++ Chapters/Resource/RefReturns/Summary.cs
@@ -0,0 +1,27 @@
+﻿namespace RefReturns.Summary
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var x = 10;
+            var y = 20;
+
+            // x, y のうち、大きい方の参照を返す。この例の場合 y を参照。
+            ref var m = ref Max(ref x, ref y);
+
+            // 参照の書き換えなので、その先の y が書き換わる。
+            m = 0;
+
+            Console.WriteLine($"{x}, {y}"); // 10, 0
+        }
+
+        static ref int Max(ref int x, ref int y)
+        {
+            if (x < y) return ref y;
+            else return ref x;
+        }
+    }
+}
---
+++ Chapters/Start/Variables/Literals.cs
@@ -0,0 +1,17 @@
+﻿namespace Variables.Literals
+{
+    using System;
+
+    class Program
+    {
+        static void M()
+        {
+            byte bitMask = 0b1100_0000;
+            Console.WriteLine(bitMask); // 192
+
+            uint magicNumber = 0xDEAD_BEEF;
+            Console.WriteLine(magicNumber); // 3735928559
+            Console.WriteLine(magicNumber.ToString("X")); // DEADBEEF 
+        }
+    }
+}
