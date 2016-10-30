ff467a53d8c4728710d8a5b6cc9e2a8ffe66bdf5
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-10-30 19:16:49 +0900

    Native Interop ANSI string

--- Chapters/Interop/NativeInterop/YourOwnDllImport.cs
+++ Chapters/Interop/NativeInterop/YourOwnDllImport.cs
@@ -6,7 +6,10 @@ namespace NativeInterop
     class YourOwnDllImport
     {
         [DllImport("Win32Dll.dll", CharSet = CharSet.Unicode)]
-        extern static void FillA(string s);
+        extern static void FillA16(string s);
+
+        [DllImport("Win32Dll.dll", CharSet = CharSet.Ansi)]
+        extern static void FillA8(string s);
 
         [DllImport("Win32Dll.dll")]
         extern static int GetValue();
@@ -14,9 +17,14 @@ namespace NativeInterop
         public static void Main()
         {
             Console.WriteLine(GetValue());
-            var s = "awsedrftgyhu";
-            FillA(s);
-            Console.WriteLine(s);
+
+            var s1 = "awsedrftgyhu";
+            FillA8(s1);
+            Console.WriteLine(s1);
+
+            var s2 = "awsedrftgyhu";
+            FillA16(s2);
+            Console.WriteLine(s2);
 
             Console.ReadKey();
         }
--- Chapters/Interop/Win32Dll/Win32Dll.cpp
+++ Chapters/Interop/Win32Dll/Win32Dll.cpp
@@ -5,11 +5,19 @@
 
 extern "C"
 {
-	__declspec(dllexport) void __stdcall FillA(wchar_t* str)
+	__declspec(dllexport) void __stdcall FillA16(wchar_t* str)
 	{
 		for (auto p = str; *p; p++)
 		{
 			*p = L'a';
+		}
+	}
+
+	__declspec(dllexport) void __stdcall FillA8(char* str)
+	{
+		for (auto p = str; *p; p++)
+		{
+			*p = 'a';
 		}
 	}
 
