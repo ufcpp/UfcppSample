2b159160b34d8e239cd66729da55f5d6f29acdb2
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-11-19 19:47:01 +0900

    native interopサンプルちょっと修正

--- Chapters/Interop/NativeInterop/YourOwnDllImport.cs
+++ Chapters/Interop/NativeInterop/YourOwnDllImport.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Runtime.InteropServices;
-
-namespace NativeInterop
+﻿namespace NativeInterop.YourOwnDllImport
 {
-    class YourOwnDllImport
+    using System;
+    using System.Runtime.InteropServices;
+
+    class Program
     {
+        // 対 UTF-16。無変換で(ポインター渡しで)呼び出せる。
         [DllImport("Win32Dll.dll", CharSet = CharSet.Unicode)]
         extern static void FillA16(string s);
 
+        // 対 ASCII。変換が必要。
         [DllImport("Win32Dll.dll", CharSet = CharSet.Ansi)]
         extern static void FillA8(string s);
 
@@ -18,13 +20,17 @@ namespace NativeInterop
         {
             Console.WriteLine(GetValue());
 
+            // 変換が必要な方。
+            // コピーが書き換わるだけなので、s1 には影響なし。
             var s1 = "awsedrftgyhu";
             FillA8(s1);
-            Console.WriteLine(s1);
+            Console.WriteLine(s1); // awsedrftgyhu
 
+            // ポインターで渡る方。
+            // s2 はネイティブ コード側での書き換えの影響を受ける。
             var s2 = "awsedrftgyhu";
             FillA16(s2);
-            Console.WriteLine(s2);
+            Console.WriteLine(s2); // aaaaaaaaaaaa
 
             Console.ReadKey();
         }
--- Chapters/Interop/Win32Dll/Win32Dll.cpp
+++ Chapters/Interop/Win32Dll/Win32Dll.cpp
@@ -5,6 +5,7 @@
 
 extern "C"
 {
+	// UTF-16 null終端文字列
 	__declspec(dllexport) void __stdcall FillA16(wchar_t* str)
 	{
 		for (auto p = str; *p; p++)
@@ -13,6 +14,7 @@ extern "C"
 		}
 	}
 
+	// ANSI null終端文字列
 	__declspec(dllexport) void __stdcall FillA8(char* str)
 	{
 		for (auto p = str; *p; p++)
