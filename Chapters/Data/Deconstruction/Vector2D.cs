3431a8fc868d172706f8bd1bef98b6445a76e32b
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-02-12 20:24:37 +0900

    Deconstruct のオーバーロード

---
+++ Chapters/Data/Deconstruction/Vector2D.cs
@@ -0,0 +1,45 @@
+﻿namespace Deconstruction._Vector2D
+{
+    using static System.Math;
+
+    struct Radian
+    {
+        public double Value { get; }
+        public Radian(double value) => Value = value;
+    }
+
+    struct Vector2D
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        // コンストラクターは当然、個数が同じでも、型が違えば呼び分けができる
+        public Vector2D(double x, double y) => (X, Y) = (x, y);
+        public Vector2D(double radius, Radian angle)
+            : this(radius * Cos(angle.Value), radius * Sin(angle.Value)) { }
+
+        // 引数の数が同じ Deconstruct が2つある
+        // 片方だけならいいけど、2つあると分解ができなくなる
+        public void Deconstruct(out double x, out double y) => (x, y) = (X, Y);
+#if false
+        public void Deconstruct(out double radius, out Radian angle)
+            => (radius, angle) = (Sqrt(X * X + Y * Y), new Radian(Atan2(Y, X)));
+#endif
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // コンストラクターの呼び分け
+            var p = new Vector2D(1, 2);
+            var q = new Vector2D(10, new Radian(PI / 5));
+
+            // 分解は呼び分けできない
+            (double x, double y) = q; // コンパイル エラー
+#if false
+            (double r, Radian a) = p; // コンパイル エラー
+#endif
+        }
+    }
+}
---
+++ Chapters/Data/Deconstruction/Vector3D.cs
@@ -0,0 +1,28 @@
+﻿using Deconstruction._Vector2D;
+
+namespace Deconstruction._Vector3D
+{
+    struct Vector3D
+    {
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+        public Vector3D(double x, double y, double z) => (X, Y, Z) = (x, y, z);
+
+        // 引数の数が違えば大丈夫
+        public void Deconstruct(out double x, out double y, out double z) => (x, y, z) = (X, Y, Z);
+        public void Deconstruct(out double first, out Vector2D rest) => (first, rest) = (X, new Vector2D(Y, Z));
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var p = new Vector3D(1, 2, 3);
+
+            // 分解可能
+            var (first, rest) = p;
+            var (x, y, z) = p;
+        }
+    }
+}
