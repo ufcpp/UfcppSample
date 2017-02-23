3a632873b6cbbf361f7dc1ba7f307a85938b2686
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-02-23 16:41:05 +0900

    サンプル: インライン展開の有無での性能差

---
+++ Demo/2017/InlineExpansion/InlinedOrNot/BenchmarkCode.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Engines;
+using System;
+
+namespace InlinedOrNot
+{
+    [SimpleJob(RunStrategy.Throughput)]
+    public class BenchmarkCode
+    {
+        static int[] testData;
+
+        static BenchmarkCode()
+        {
+            var r = new Random();
+            testData = new int[100];
+
+            for (int i = 0; i < testData.Length; i++)
+            {
+                testData[i] = r.Next();
+            }
+        }
+
+        [Benchmark]
+        public static int ManuallyInlined()
+        {
+            var sum = 0;
+            foreach (var x in testData) sum = sum + x;
+            return sum;
+        }
+
+        [Benchmark]
+        public static int Inlining()
+        {
+            var sum = 0;
+            foreach (var x in testData) sum = Target.Inlining(sum, x);
+            return sum;
+        }
+
+        [Benchmark]
+        public static int NoInlining()
+        {
+            var sum = 0;
+            foreach (var x in testData) sum = Target.NoInlining(sum, x);
+            return sum;
+        }
+    }
+}
---
+++ Demo/2017/InlineExpansion/InlinedOrNot/Target.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace InlinedOrNot
+{
+    /// <summary>
+    /// 計測・比較したい対象。
+    /// </summary>
+    public class Target
+    {
+        // 普通に定義。このくらいの中身だと確実にインライン展開される
+        public static int Inlining(int x, int y) => x + y;
+
+        // インライン展開をわざわざ抑止
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static int NoInlining(int x, int y) => x + y;
+    }
+}
