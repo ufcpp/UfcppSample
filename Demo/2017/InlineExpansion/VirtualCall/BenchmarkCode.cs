6d78eb343b118d50db7f69b3a98809b4848046f7
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-02-23 17:16:08 +0900

    サンプル: 仮想呼び出しとインライン展開

---
+++ Demo/2017/InlineExpansion/VirtualCall/BenchmarkCode.cs
@@ -0,0 +1,63 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Engines;
+using System;
+
+namespace VirtualCall
+{
+    [SimpleJob(RunStrategy.Throughput)]
+    public class BenchmarkCode
+    {
+        static A a = new A();
+        static B b = new B();
+        const int Loops = 100;
+
+        [Benchmark]
+        public static int DirectA()
+        {
+            var sum = 0;
+            for (int i = 0; i < Loops; i++) sum += Target.CallA(a);
+            return sum;
+        }
+
+        [Benchmark]
+        public static int DirectB()
+        {
+            var sum = 0;
+            for (int i = 0; i < Loops; i++) sum += Target.CallB(b);
+            return sum;
+        }
+
+        [Benchmark]
+        public static int InterfaceA()
+        {
+            var sum = 0;
+            for (int i = 0; i < Loops; i++) sum += Target.CallInterface(a);
+            return sum;
+        }
+
+        [Benchmark]
+        public static int InterfaceB()
+        {
+            var sum = 0;
+            for (int i = 0; i < Loops; i++) sum += Target.CallInterface(b);
+            return sum;
+        }
+
+        [Benchmark]
+        public static int GenericA()
+        {
+            var sum = 0;
+            for (int i = 0; i < Loops; i++) sum += Target.Call(a);
+            return sum;
+        }
+
+        [Benchmark]
+        public static int GenericB()
+        {
+            var sum = 0;
+            for (int i = 0; i < Loops; i++) sum += Target.Call(b);
+            return sum;
+        }
+    }
+}
---
+++ Demo/2017/InlineExpansion/VirtualCall/Target.cs
@@ -0,0 +1,31 @@
+﻿namespace VirtualCall
+{
+    interface ISample
+    {
+        int Value { get; }
+    }
+
+    class A : ISample
+    {
+        public int Value => 1;
+    }
+
+    class B : ISample
+    {
+        // B の方にだけ virtual を付ける
+        public virtual int Value => 1;
+    }
+
+    class Target
+    {
+        // A, B の Value を直接呼ぶ
+        public static int CallA(A x) => x.Value;
+        public static int CallB(B x) => x.Value;
+
+        // インターフェイス越しに Value を呼ぶ
+        public static int CallInterface(ISample x) => x.Value;
+
+        // ジェネリック越しに Value を呼ぶ
+        public static int Call<T>(T x) where T : ISample => x.Value;
+    }
+}
