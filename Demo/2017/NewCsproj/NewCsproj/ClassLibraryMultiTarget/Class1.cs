b2bbb7265a5dd482cccc891a87374703ef271d22
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-05-12 19:58:13 +0900

    マルチターゲットな新csproj

---
+++ Demo/2017/NewCsproj/NewCsproj/ClassLibraryMultiTarget/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibraryStd20
+{
+    public class Class1
+    {
+#if NET35
+        public static string Name => ".NET Framework 3.5";
+#elif NETSTANDARD1_0
+        public static string Name => ".NET Standard 1.0";
+#endif
+    }
+}
