e232f8d8c545baeec87ce4375b03b4a69ac23bd0
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-05-12 19:22:27 +0900

    新旧 csproj

---
+++ Demo/2017/NewCsproj/NewCsproj/ConsoleAppNet35/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppCore11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Demo/2017/NewCsproj/OldCsproj/ConsoleAppNet35/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppNet35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
