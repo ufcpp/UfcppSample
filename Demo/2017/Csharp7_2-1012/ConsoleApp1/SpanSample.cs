7f9b548936eb2952ce7fe443a0cca6d3532cc8c1
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-10-12 15:34:38 +0900

    Span を使った配列の deconstruction の例

---
+++ Demo/2017/Csharp7_2-1012/ConsoleApp1/SpanSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SpanSample
+{
+    static class Ex
+    {
+        public static void Deconstruct<T>(this T[] array, out T head, out Span<T> rest) => new Span<T>(array).Deconstruct(out head, out rest);
+
+        public static void Deconstruct<T>(this Span<T> span, out T head, out Span<T> rest)
+        {
+            head = span[0];
+            rest = span.Slice(1);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var array = new[] { 1, 2, 3, 4, 5 };
+
+            var (x, (y, (z, rest))) = array;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine("rest length: " + rest.Length);
+        }
+    }
+}
