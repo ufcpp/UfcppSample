7a9ba99a2d0bb0f53e5d47fc3658123e238aaffd
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-12-10 13:34:26 +0900

    bug report: operator with in parameters

---
+++ Demo/2017/InOperatorDifferentAssemly/ClassLibrary1/Class1.cs
@@ -0,0 +1,34 @@
+﻿namespace ClassLibrary1
+{
+    public struct DifferentAssembly
+    {
+        public double W;
+        public double X;
+        public double Y;
+        public double Z;
+
+        public DifferentAssembly(double w, double x, double y, double z)
+        {
+            W = w;
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        // by-val
+        public static DifferentAssembly operator +(DifferentAssembly a, DifferentAssembly b)
+            => new DifferentAssembly(
+                a.W + b.W,
+                a.X + b.X,
+                a.Y + b.Y,
+                a.Z + b.Z);
+
+        // by-ref-readonly
+        public static DifferentAssembly operator *(in DifferentAssembly a, in DifferentAssembly b)
+            => new DifferentAssembly(
+                a.W * b.W - a.X * b.X - a.Y * b.Y - a.Z * b.Z,
+                a.W * b.X + a.X * b.W + a.Y * b.Z - a.Z * b.Y,
+                a.W * b.Y + a.Y * b.W + a.Z * b.X - a.X * b.Z,
+                a.W * b.Z + a.Z * b.W + a.X * b.Y - a.Y * b.X);
+    }
+}
---
+++ Demo/2017/InOperatorDifferentAssemly/ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Legal();
+            Error();
+        }
+
+        private static void Legal()
+        {
+            var a = new SameAssembly(1, 2, 3, 4);
+            var b = new SameAssembly(1, -2, 3, -4);
+
+            // No error.
+            Console.WriteLine(a + b);
+
+            // No error.
+            Console.WriteLine(a * b);
+        }
+
+        private static void Error()
+        {
+            var a = new DifferentAssembly(1, 2, 3, 4);
+            var b = new DifferentAssembly(1, -2, 3, -4);
+
+            // pass by value. No error.
+            Console.WriteLine(a + b);
+
+            // pass by ref readonly.
+            // No error in the Visual Studio C# Editor
+            // but error on build.
+            Console.WriteLine(a * b);
+        }
+    }
+
+    /// <summary>
+    /// The same as <see cref="ClassLibrary1.DifferentAssembly"/>.
+    /// </summary>
+    public struct SameAssembly
+    {
+        public double W;
+        public double X;
+        public double Y;
+        public double Z;
+
+        public SameAssembly(double w, double x, double y, double z)
+        {
+            W = w;
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        // by-val
+        public static SameAssembly operator +(SameAssembly a, SameAssembly b)
+            => new SameAssembly(
+                a.W + b.W,
+                a.X + b.X,
+                a.Y + b.Y,
+                a.Z + b.Z);
+
+        // by-ref-readonly
+        public static SameAssembly operator *(in SameAssembly a, in SameAssembly b)
+            => new SameAssembly(
+                a.W * b.W - a.X * b.X - a.Y * b.Y - a.Z * b.Z,
+                a.W * b.X + a.X * b.W + a.Y * b.Z - a.Z * b.Y,
+                a.W * b.Y + a.Y * b.W + a.Z * b.X - a.X * b.Z,
+                a.W * b.Z + a.Z * b.W + a.X * b.Y - a.Y * b.X);
+    }
+}
