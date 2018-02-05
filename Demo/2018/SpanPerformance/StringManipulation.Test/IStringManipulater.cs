530eee225d3dcb348db224a7976e5265e139bd7c
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-02-06 01:12:09 +0900

    add xunit テスト

---
+++ Demo/2018/SpanPerformance/StringManipulation.Test/IStringManipulater.cs
@@ -0,0 +1,42 @@
+﻿namespace StringManipulation
+{
+    /// <summary>
+    /// テスト用。
+    /// <see cref="Classic.StringExtensions"/>
+    /// <see cref="Unsafe.StringExtensions"/>
+    /// <see cref="SafeStackalloc.StringExtensions"/>
+    /// の静的メソッドを多態化するために使う。
+    /// </summary>
+    interface IStringManipulater
+    {
+        string SnakeToCamel(string s);
+        string CamelToSnake(string s);
+    }
+
+    namespace Classic
+    {
+        struct Manipulater : IStringManipulater
+        {
+            public string CamelToSnake(string s) => s.CamelToSnake();
+            public string SnakeToCamel(string s) => s.SnakeToCamel();
+        }
+    }
+
+    namespace Unsafe
+    {
+        struct Manipulater : IStringManipulater
+        {
+            public string CamelToSnake(string s) => s.CamelToSnake();
+            public string SnakeToCamel(string s) => s.SnakeToCamel();
+        }
+    }
+
+    namespace SafeStackalloc
+    {
+        struct Manipulater : IStringManipulater
+        {
+            public string CamelToSnake(string s) => s.CamelToSnake();
+            public string SnakeToCamel(string s) => s.SnakeToCamel();
+        }
+    }
+}
---
+++ Demo/2018/SpanPerformance/StringManipulation.Test/Test.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+
+namespace StringManipulation
+{
+    public class StringManipulationTest
+    {
+        IStringManipulater[] targets = new IStringManipulater[]
+        {
+            new Classic.Manipulater(),
+            new Unsafe.Manipulater(),
+            new SafeStackalloc.Manipulater(),
+        };
+
+        [Theory]
+        [InlineData("ABCDEFGHIJKLMNOPQRSTUVWXYZ")]
+        [InlineData("DeriveRestrictedAppContainerSidFromAppContainerSidAndRestrictedName")]
+        [InlineData("InternalFrameInternalFrameTitlePaneInternalFrameTitlePaneMaximizeButtonWindowNotFocusedState")]
+        public void CamelToSnakeToCamel(string s)
+        {
+            foreach (var t in targets) AssertCamelToSnakeToCamel(s, t);
+        }
+
+        void AssertCamelToSnakeToCamel(string s, IStringManipulater m)
+        {
+            var snake = m.CamelToSnake(s);
+            var camel = m.SnakeToCamel(snake);
+            Assert.Equal(s, camel);
+        }
+
+        [Theory]
+        [InlineData("abcdefghijklmnopqrstuvwxyz")]
+        [InlineData("a_b_c_d_e_f_g_h_i_j_k_l_m_n_o_p_q_r_s_t_u_v_w_x_y_z")]
+        [InlineData("ensure_name_not_reserved")]
+        public void SnakeToCamelToSnake(string s)
+        {
+            foreach (var t in targets) AssertSnakeToCamelToSnake(s, t);
+        }
+
+        void AssertSnakeToCamelToSnake(string s, IStringManipulater m)
+        {
+            var camel = m.SnakeToCamel(s);
+            var snake = m.CamelToSnake(camel);
+            Assert.Equal(s, snake);
+        }
+    }
+}
