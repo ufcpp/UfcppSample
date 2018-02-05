77b00c5c6e8f429937b17cd4aeab80156071e1f4
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-02-06 02:04:20 +0900

    Benchmark

---
+++ Demo/2018/SpanPerformance/StringManipulation.Benchmark/Benchmark.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace StringManipulation.Benchmark
+{
+    public class StringManipulationBenchmark
+    {
+        string[] _data = new[]
+        {
+            "a",
+            "ab",
+            "abc",
+            "a_b_c",
+            "A",
+            "AB",
+            "ABC",
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            "DeriveRestrictedAppContainerSidFromAppContainerSidAndRestrictedName",
+            "InternalFrameInternalFrameTitlePaneInternalFrameTitlePaneMaximizeButtonWindowNotFocusedState",
+            "abcdefghijklmnopqrstuvwxyz",
+            "a_b_c_d_e_f_g_h_i_j_k_l_m_n_o_p_q_r_s_t_u_v_w_x_y_z",
+            "ensure_name_not_reserved",
+        };
+
+        [Benchmark] public void Classic() => Run<Classic.Manipulater>();
+        [Benchmark] public void Unsafe() => Run<Unsafe.Manipulater>();
+        [Benchmark] public void SafeStackalloc() => Run<SafeStackalloc.Manipulater>();
+
+        void Run<M>()
+            where M : struct, IStringManipulater
+        {
+            foreach (var x in _data)
+            {
+                var s = default(M).CamelToSnake(x);
+                var c = default(M).SnakeToCamel(x);
+                var s1 = default(M).CamelToSnake(c);
+                var c1 = default(M).SnakeToCamel(s);
+            }
+        }
+    }
+}
---
+++ Demo/2018/SpanPerformance/StringManipulation.Benchmark/Program.cs
@@ -0,0 +1,76 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.CsProj;
+using BenchmarkDotNet.Toolchains.DotNetCli;
+using System.Diagnostics;
+
+namespace StringManipulation.Benchmark
+{
+    class Program
+    {
+        static void Main()
+        {
+            // ReadOnlySpan NonPortableCast が「Generate Exception: Method not found」とか言って怒られて実行できない。
+            // Span 絡みの API 変更入りまくってるから完全にバージョンが一致してないとたぶんこの手のエラーで死ぬ。
+            // 2・3時間悩んで解決できなかったんで Span のシグネチャが安定するまでしばらく塩漬け…
+            //BenchmarkRunner.Run<StringManipulationBenchmark>(new MyConfig());
+
+            // 仕方なく Stopwatch 運用…
+            const int N = 5000;
+            var benchmark = new StringManipulationBenchmark();
+
+            var sw = new Stopwatch();
+
+            sw.Start();
+            for (int i = 0; i < N; i++)
+            {
+                benchmark.Classic();
+            }
+            sw.Stop();
+            System.Console.WriteLine(sw.Elapsed);
+            sw.Reset();
+
+            sw.Start();
+            for (int i = 0; i < N; i++)
+            {
+                benchmark.Unsafe();
+            }
+            sw.Stop();
+            System.Console.WriteLine(sw.Elapsed);
+            sw.Reset();
+
+            sw.Start();
+            for (int i = 0; i < N; i++)
+            {
+                benchmark.SafeStackalloc();
+            }
+            sw.Stop();
+            System.Console.WriteLine(sw.Elapsed);
+            sw.Reset();
+        }
+    }
+
+    public class MyConfig : ManualConfig
+    {
+        public MyConfig()
+        {
+            Add(Job.Default.With(
+                CsProjCoreToolchain.From(
+                    new NetCoreAppSettings(
+                        targetFrameworkMoniker: "netcoreapp2.1",
+                        runtimeFrameworkVersion: "2.1.0-preview2-26130-06",
+                        name: ".NET Core 2.1"))));
+
+            Add(DefaultColumnProviders.Instance);
+            Add(MarkdownExporter.GitHub);
+            Add(MemoryDiagnoser.Default);
+            Add(new ConsoleLogger());
+            Add(new HtmlExporter());
+        }
+    }
+}
