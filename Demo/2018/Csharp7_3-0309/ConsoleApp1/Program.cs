4e52e5c44d6283b84b97e01f61f5f9e86cee70c5
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-03-30 00:45:07 +0900

    updated todo comment

--- Demo/2018/Csharp7_3-0309/ConsoleApp1/Program.cs
+++ Demo/2018/Csharp7_3-0309/ConsoleApp1/Program.cs
@@ -2,17 +2,22 @@
 
 /*
 todo:
-- [ ] blittable	None	Merged	None		jaredpar
-- [ ] Support == and != for tuples	tuple-equality	Merged	jcouv	AlekseyTs	jcouv
-- [ ] strongname	master	Merged	tyoverby	agocke	jaredpar
-- [x] Attribute on backing field	features/compiler	Merged	jcouv	AlekseyTs	jcouv
-- [x] Ref Reassignment	ref-reassignment	Merged	agocke	vsadov	jarepdar
-- [ ] Constraints	constraints	Merged	OmarTawfik	vsadov	jarepdar
-- [ ] Stackalloc initializers	stackalloc-init	Merged	alrz	vsadov	jcouv
-- [ ] Custom fixed	custom-fixed	Merged	vsadov	jcouv	jarepdar
-- [ ] Indexing movable fixed buffers		Merged	vsadov	jcouv	jarepdar
-- [ ] Improved overload candidates	features/compiler	Merged	gafter	cston	mattwar
-- [ ] Expression variables	features/ExpressionVariables	Merged	AlekseyTs	gafter	gafter
+- generic constraints
+  - [x] unmanaged
+  - [ ] Enum
+  - [ ] Delegate
+- [ ] Support == and != for tuples
+- [ ] strongname
+- [x] Attribute on backing field
+- [x] Ref Reassignment
+- [ ] Stackalloc initializers
+- [ ] Custom fixed	custom-fixed
+- [ ] Indexing movable fixed buffers
+- Improved overload candidates
+  - [ ] generic constraints
+  - [ ] static/instance
+  - [ ] method group return type
+- [ ] Expression variables
  */
 
 namespace ConsoleApp1
