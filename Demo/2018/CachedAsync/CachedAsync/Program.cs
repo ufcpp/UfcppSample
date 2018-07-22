41ac3bc785a32ae2a0fbee79601d576623956ad0
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-07-22 11:17:41 +0900

    CachedAsync
    まずは https://github.com/dotnet/corefx/blob/master/src/System.Threading.Channels/src/System/Threading/Channels/AsyncOperation.cs ほぼそのまま。

---
+++ Demo/2018/CachedAsync/CachedAsync/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CachedAsync
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var x = new TimerLoop(500);
+            await ReadAsync(x);
+            await x.StopAsync();
+        }
+
+        static async Task ReadAsync(TimerLoop x)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                var res = await x.XAsync();
+
+                var e = Math.Round(sw.ElapsedMilliseconds / 10.0) / 100;
+                Console.WriteLine((res, e, Thread.CurrentThread.ManagedThreadId));
+            }
+            sw.Stop();
+        }
+    }
+}
---
+++ Demo/2018/CachedAsync/CachedAsync/TimerLoop.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CachedAsync
+{
+    class TimerLoop
+    {
+        private readonly AsyncOperation<int> _cache = new AsyncOperation<int>(false, pooled: true);
+        private readonly int _intervalMillisecond;
+        private readonly CancellationTokenSource _cts;
+        private readonly Task _loop;
+
+        public TimerLoop(int intervalMillisecond)
+        {
+            _intervalMillisecond = intervalMillisecond;
+            _cts = new CancellationTokenSource();
+            _loop = Loop();
+        }
+
+        private async Task Loop()
+        {
+            var i = 0;
+            while (!_cts.IsCancellationRequested)
+            {
+                await Task.Delay(_intervalMillisecond);
+                _cache.TrySetResult(i);
+                ++i;
+            }
+        }
+
+        public Task StopAsync()
+        {
+            _cts.Cancel();
+            return _loop;
+        }
+
+        void Throw() => throw new InvalidOperationException();
+
+        public ValueTask<int> XAsync()
+        {
+            if (!_cache.TryOwnAndReset()) Throw();
+            return _cache.ValueTaskOfT;
+        }
+    }
+}
