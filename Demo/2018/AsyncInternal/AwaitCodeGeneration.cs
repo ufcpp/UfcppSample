1d7e250c56c8ef49ed3e0df089cea6fafbe4cd75
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-08-05 15:32:21 +0900

    await 置き換え Step3

--- Demo/2018/AsyncInternal/AwaitCodeGeneration.cs
+++ Demo/2018/AsyncInternal/AwaitCodeGeneration.cs
@@ -6,21 +6,29 @@ namespace AsyncInternal
 {
     class AwaitCodeGeneration
     {
-        // Step2: メソッド全体を包む匿名なクラスを生成(ここではラムダ式で代用)
+        // Step3: state 導入。await のところに state 更新と goto 用ラベルを追加
         public static async Task<IEnumerable<string>> GetContents()
         {
+            var state = 0;
+
             void a()
             {
+                state = 1;
                 var indexes = await GetIndex();
+                Case1:
 
+                state = 1;
                 var selectedIndexes = await SelectIndex(indexes);
+                Case2:
 
                 var contents = new List<string>();
                 var e = selectedIndexes.GetEnumerator();
 
                 goto EndLoop;
                 BeginLoop:
+                state = 1;
                 var content = await GetContent(e.Current);
+                Case3:
                 contents.Add(content);
                 EndLoop:
                 if (e.MoveNext()) goto BeginLoop;
