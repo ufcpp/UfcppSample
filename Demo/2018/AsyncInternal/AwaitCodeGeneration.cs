e345e2ad8dc41220a9c30613c48b408ffe7faa9d
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-08-05 15:50:23 +0900

    await 最適化

--- Demo/2018/AsyncInternal/AwaitCodeGeneration.cs
+++ Demo/2018/AsyncInternal/AwaitCodeGeneration.cs
@@ -6,7 +6,7 @@ namespace AsyncInternal
 {
     class AwaitCodeGeneration
     {
-        // Step7: 戻り値は TaskCompletionSouce 越しに返す
+        // 最適化: 未完了 Task だけを ContinueWith、終わったあと null 上書き
         public static Task<IEnumerable<string>> GetContents()
         {
             var state = 0;
@@ -29,17 +29,25 @@ namespace AsyncInternal
                 }
                 state = 1;
                 tIndexes = GetIndex();
-                tIndexes.ContinueWith(_ => a());
-                return;
+                if (!tIndexes.IsCompleted)
+                {
+                    tIndexes.ContinueWith(_ => a());
+                    return;
+                }
                 Case1:
                 var indexes = tIndexes.Result;
+                tIndexes = null;
 
                 state = 2;
                 tSelectedIndexes = SelectIndex(indexes);
-                tSelectedIndexes.ContinueWith(_ => a());
-                return;
+                if (!tSelectedIndexes.IsCompleted)
+                {
+                    tSelectedIndexes.ContinueWith(_ => a());
+                    return;
+                }
                 Case2:
                 var selectedIndexes = tSelectedIndexes.Result;
+                tSelectedIndexes = null;
 
                 contents = new List<string>();
                 e = selectedIndexes.GetEnumerator();
@@ -48,10 +56,15 @@ namespace AsyncInternal
                 BeginLoop:
                 state = 3;
                 tContent = GetContent(e.Current);
-                tContent.ContinueWith(_ => a());
-                return;
+                if (!tContent.IsCompleted)
+                {
+                    tContent.ContinueWith(_ => a());
+                    return;
+                }
                 Case3:
                 var content = tContent.Result;
+                tContent = null;
+
                 contents.Add(content);
                 EndLoop:
                 if (e.MoveNext()) goto BeginLoop;
