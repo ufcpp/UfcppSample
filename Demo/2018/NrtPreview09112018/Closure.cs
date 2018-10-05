b8f30f656472eea0b644488c5e81559961fc5cfd
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-10-05 20:02:36 +0900

    NRT 追加

---
+++ Demo/2018/NrtPreview09112018/Closure.cs
@@ -0,0 +1,17 @@
+﻿class Closure
+{
+    static void X(object ? x)
+    {
+        if (x == null)
+        {
+            x = new object();
+        }
+
+        void a()
+        {
+            object nonNull = x; // CS8600
+        };
+
+        a();
+    }
+}
