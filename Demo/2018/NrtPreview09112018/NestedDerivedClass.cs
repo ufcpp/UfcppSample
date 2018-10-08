eee3201d4415a96baed3560f93afa1ae7d4aa612
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-10-08 15:12:53 +0900

    NRT 入れ子の派生型

---
+++ Demo/2018/NrtPreview09112018/NestedDerivedClass.cs
@@ -0,0 +1,19 @@
+﻿interface I<T>
+{
+    T[]? X { get; }
+}
+
+abstract class Base<T> : I<T>
+{
+    public virtual T[]? X => null;
+
+    class Inner : Base<T>
+    {
+        public override T[]? X => null; // CS8608
+    }
+}
+
+abstract class Base : Base<string>
+{
+    public override string[]? X => null; // No warning
+}
