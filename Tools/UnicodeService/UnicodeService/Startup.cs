b6a1f4ad6567d8a8b2fa364322a66cd0071d6601
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-10-20 23:22:12 +0900

    Update UnicodeService

--- Tools/UnicodeService/UnicodeService/Startup.cs
+++ Tools/UnicodeService/UnicodeService/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,7 +26,6 @@ namespace UnicodeService
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {
