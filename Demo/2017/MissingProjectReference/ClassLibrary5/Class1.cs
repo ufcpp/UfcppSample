005848d8c1ba6c3247a47dc3a09d2f178e0dc120
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-11-03 22:57:16 +0900

    これももう役目終えてるはず

--- Demo/2017/MissingProjectReference/ClassLibrary5/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ClassLibrary5
-{
-    public class Class1
-    {
-    }
-}
--- Demo/2017/MissingProjectReference/ClassLibrary6B/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ClassLibrary6B
-{
-    public class Class1
-    {
-    }
-}
