2fb2e6b5bda47427d4eb6b8b06da914f6523df65
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-11-05 12:46:34 +0900

    enumeration

---
+++ Demo/2018/PerformanceTips/Enumeration/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Enumeration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<EnumerationBenchmark>();
+        }
+    }
+}
