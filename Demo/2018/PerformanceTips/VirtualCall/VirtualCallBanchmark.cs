c49d72455fdda78c80d07ad3070fda313f4a81c4
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-11-11 01:02:57 +0900

    VirtualCall

---
+++ Demo/2018/PerformanceTips/VirtualCall/VirtualCallBanchmark.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+interface IValue { int Value { get; } }
+class Impl : IValue { public int Value => 0; }
+
+public class VirtualCallBanchmark
+{
+    // インターフェイス越し
+    IValue A { get; } = new Impl();
+
+    // クラスを直公開
+    Impl B { get; } = new Impl();
+
+    [Benchmark]
+    public int Interface() => A.Value;
+
+    [Benchmark]
+    public int Class() => B.Value;
+}
