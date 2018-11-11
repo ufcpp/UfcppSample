c2ba48b963245f962ae6d531ffb3c7abb80b37ea
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-11-11 16:05:03 +0900

    EqualityComparerDefault

---
+++ Demo/2018/PerformanceTips/EqualityComparerDefault/Program.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+
+public struct Unit : IEquatable<Unit>
+{
+    public bool Equals(Unit other) => true;
+    public override bool Equals(object obj) => obj is Unit;
+    public override int GetHashCode() => 0;
+}
+
+public class EqualityComparerDefaultBenchmark
+{
+    [Benchmark]
+    public bool IntEquals() => EqualityComparer<int>.Default.Equals(1, 2);
+
+    [Benchmark]
+    public bool StringEquals() => EqualityComparer<string>.Default.Equals("abc", "def");
+
+    [Benchmark]
+    public bool UserDefinedEquals() => EqualityComparer<Unit>.Default.Equals(default, default);
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<EqualityComparerDefaultBenchmark>(new MultipleRuntimesConfig());
+    }
+}
