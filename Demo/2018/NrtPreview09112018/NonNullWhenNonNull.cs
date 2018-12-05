4fe0f830fcbbdee427ebeaf173d11de646783921
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-12-05 15:31:08 +0900

    NonNullWhenNonNull

---
+++ Demo/2018/NrtPreview09112018/NonNullWhenNonNull.cs
@@ -0,0 +1,29 @@
+﻿namespace NrtPreview09112018
+{
+    static class NonNullWhenNonNull
+    {
+        class Base { }
+        class Derived : Base { }
+
+        static void M()
+        {
+            Base[] b = new Derived[] { new Derived() };
+            Derived[] d = b.Convert<Base, Derived>(); // expected non-null bacause b is not null, but CS8600
+        }
+
+        public static TDerived[]? Convert<TBase, TDerived>(this 
+            // I want [NonNullWhenNonNull] attribute.
+            TBase[]? array)
+            where TDerived : class, TBase
+        {
+            if (array is TDerived[] x) return x;
+            if (array == null) return null;
+
+            TDerived[] d = new TDerived[array.Length];
+            TBase[] b = d;
+            for (int i = 0; i < d.Length; i++)
+                b[i] = array[i];
+            return d;
+        }
+    }
+}
