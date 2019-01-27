32ee3f57faeaacc8fcce4e9df9bf3b4902bfc10b
Nobuyuki Iwanaga <ufcpp@live.jp>
2019-01-27 16:30:08 +0900

    ローカル関数のshadowing

---
+++ Demo/2019/Csharp80/Preview2/Shadowing.cs
@@ -0,0 +1,22 @@
+﻿namespace Preview2.Shadowing
+{
+    class Program
+    {
+        static void Main()
+        {
+            // ローカル関数内の引数・変数、その上位スコープと同じ名前を付けれるようになったみたい。
+            // (上位スコープのやつを隠しちゃうので、shadowing って言う。)
+            var (x, y) = (1, 2);
+
+            // C# 7.3 までは、この引数とか変数の x, y でエラーになってた
+            int a(int x, int y) => x + y;
+
+            // キャプチャした変数なのかシャドーイングした変数なのかで紛らわしいので、
+            // 使うなら static ローカル関数(キャプチャできない)とセットで使うべきかも。
+            static int b() { int x = 0; return x; }
+
+            a(x, y);
+            b();
+        }
+    }
+}
