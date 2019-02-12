a51061ebd9b3f1a0c536f919a06fee1bb716b9d9
Nobuyuki Iwanaga <ufcpp@live.jp>
2019-02-12 22:24:26 +0900

    非 null チェック、Deconstruct 削減

---
+++ Chapters/Data/Patterns/CallOnce.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns.CallOnce
+{
+    class X
+    {
+        public int Value { get; }
+        public X(int value) => Value = value;
+        public void Deconstruct(out int value) => value = Value;
+    }
+
+    class Program
+    {
+        static int M(X x)
+            => x switch
+        {
+            // 引数の数が同じ位置パターンを3回。
+            // この場合、Deconstruct(out int) の呼び出しは1回にまとめられる。
+            (0) _ => 1,
+            (1) _ => 2,
+            (2) _ => 0,
+            _ => x.Value
+        };
+    }
+}
---
+++ Chapters/Data/Patterns/NonNull.cs
@@ -0,0 +1,36 @@
+﻿namespace Patterns.NonNull
+{
+    class Program
+    {
+        struct LongLongNamedStruct { }
+
+        void M1(LongLongNamedStruct? x)
+        {
+            // こういう書き方だと null チェックになる。
+            if (x is LongLongNamedStruct nonNull)
+            {
+                // obj が null じゃない時だけここが実行される。
+                // でも、x の型が既知なのに、長いクラス名をわざわざ書くのはしんどい…
+            }
+        }
+
+        void M2(LongLongNamedStruct? x)
+        {
+            // が、var パターンは null にもマッチしちゃう。
+            // (var は「何にでもマッチ」。null でも true になっちゃう。)
+            if (x is var nullable)
+            {
+                // obj が null でもここが実行される。
+            }
+        }
+
+        void M3(LongLongNamedStruct? x)
+        {
+            // (C# 8.0) プロパティ パターンであれば、null チェックを含む。
+            if (x is { } nonNull)
+            {
+                // obj が null じゃない時だけここが実行される。
+            }
+        }
+    }
+}
