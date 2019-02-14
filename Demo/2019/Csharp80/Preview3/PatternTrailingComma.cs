8d5dbc556e1b868bfb33ed4f5f5c2e7695e1e857
Nobuyuki Iwanaga <ufcpp@live.jp>
2019-02-14 21:20:20 +0900

    PatternTrailingComma

---
+++ Demo/2019/Csharp80/Preview3/PatternTrailingComma.cs
@@ -0,0 +1,12 @@
+﻿namespace Preview3.PatternTrailingComma
+{
+    class Program
+    {
+        static bool M((int a, int b) t)
+            => t is
+        {
+            a: 1,
+            b: 2, // この、最後の1個の , が OK になった。
+        };
+    }
+}
