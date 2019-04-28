8e6a343dc71e6efbcd0b7ceef8556e736692781b
Nobuyuki Iwanaga <ufcpp@live.jp>
2019-04-28 16:08:36 +0900

    readonly members

---
+++ Demo/2019/Csharp80/VS16_1_p2/ReadOnlyMember.cs
@@ -0,0 +1,38 @@
+﻿namespace VS16_1_p2.ReadOnlyMember
+{
+    // 構造体自体は readonly にしない。
+    // フィールドは書き換えたい
+    struct NonReadOnly
+    {
+        public float X;
+        public float Y;
+
+        // でも、このプロパティ内ではフィールドを書き換えない
+        public float LengthSquared => X * X + Y * Y;
+    }
+
+    // NonReadOnly との差は LengthSquared の readonly の有無だけ
+    struct ReadOnly
+    {
+        public float X;
+        public float Y;
+
+        // readonly 修飾でフィールドを書き換えないことを明示
+        public readonly float LengthSquared => X * X + Y * Y;
+    }
+
+    class Program
+    {
+        // こっちは、LengthSquared 内での X, Y の書き換えを恐れて隠れたコピーが発生する。
+        static float M(in NonReadOnly x) => x.LengthSquared;
+
+        // こっちは、LengthSquared に readonly が付いているのでコピー発生しない。
+        static float M(in ReadOnly x) => x.LengthSquared;
+
+        static void Main(string[] args)
+        {
+            M(new NonReadOnly { X = 1, Y = 2 });
+            M(new ReadOnly { X = 1, Y = 2 });
+        }
+    }
+}
---
+++ Demo/2019/Csharp80/VS16_1_p2/ReadOnlyRef.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VS16_1_p2.ReadOnlyRef
+{
+    struct S
+    {
+        public int[] _value;
+
+        // これは、読み取り専用参照を返すという意味。
+        // _value 配列の中身が書き換わってもらっては困る。
+        public ref readonly int X => ref _value[0];
+
+        // これは、S 内のフィールド(この場合 _value) を書き換えないという意味。
+        // _value 配列の中身が書き換わろうと知ったことではない。
+        public readonly ref int Y => ref _value[0];
+
+        // これは、上記2つの両方の意味。
+        // _value 自体も書き換わらないし、_value の中身を書き換えてもらっても困るとき用。
+        public readonly ref readonly int Z => ref _value[0];
+    }
+
+    class Program
+    {
+    }
+}
