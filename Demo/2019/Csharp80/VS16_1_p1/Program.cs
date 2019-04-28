51da7b0755a25e60e326820cb2ce531e108228f1
Nobuyuki Iwanaga <ufcpp@live.jp>
2019-04-28 15:25:29 +0900

    VS 16.1 Preview 1

---
+++ Demo/2019/Csharp80/VS16_1_p1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VS16_1_p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Demo/2019/Csharp80/VS16_1_p1/UnmanagedGenericStruct.cs
@@ -0,0 +1,37 @@
+﻿namespace VS16_1_p1.UnmanagedGenericStruct
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void SafeStackalloc<T>()
+            where T : unmanaged
+        {
+            Span<T> span = stackalloc T[4];
+        }
+
+        static void Main()
+        {
+            // 以前から OK
+            SafeStackalloc<int>(); // 値型
+            SafeStackalloc<DateTime>(); // 値型だけを含む構造体
+
+#if false
+            // 以下はNG
+            SafeStackalloc<string>(); // 参照型
+#endif
+
+            // C# 8.0 で OK になった (ジェネリックな型、入れ子も OK)
+            SafeStackalloc<KeyValuePair<int, int>>();
+            SafeStackalloc<Wrap<int>>();
+            SafeStackalloc<KeyValuePair<(float, bool), Wrap<int>>>();
+        }
+
+        struct Wrap<T>
+            where T : unmanaged
+        {
+            public T Value;
+        }
+    }
+}
