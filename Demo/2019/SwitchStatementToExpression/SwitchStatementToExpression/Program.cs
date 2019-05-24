5d9861301fcd48afed8c78aee76af66a2df2721b
Nobuyuki Iwanaga <ufcpp@live.jp>
2019-05-24 18:47:52 +0900

    Switch Statement

---
+++ Demo/2019/SwitchStatementToExpression/SwitchStatementToExpression/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SwitchStatementToExpression
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(M(true));
+            Console.WriteLine(M(false));
+        }
+
+        static int M(bool x)
+        {
+            switch(x)
+            {
+                case true: return 1;
+                case false: return 0;
+            }
+        }
+    }
+}
