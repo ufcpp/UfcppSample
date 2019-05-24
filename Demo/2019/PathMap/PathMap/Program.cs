2a72a771cc117b5bc749dbd4c56eff01393685f7
Nobuyuki Iwanaga <ufcpp@live.jp>
2019-05-24 22:36:54 +0900

    PathMap の例

---
+++ Demo/2019/PathMap/PathMap/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace PathMap
+{
+    class Program
+    {
+        static void Main()
+        {
+            // CallerFilePath で、このファイルのパスがコンソール出力される。
+            // 通常ならフルパス。
+            // 今回は csproj に $(MSBuildProjectDirectory)=. と PathMap 設定を入れているので、
+            // .\Program.cs
+            // と表示される。
+            M();
+
+            // スタックトレースにも影響。
+            try
+            {
+                Throw();
+            }
+            catch (Exception ex)
+            {
+                // これも、フルパスだったものが短いスタックトレースに変わる:
+                //.\Program.cs
+                //   at PathMap.Program.Throw() in .\Program.cs:line 33
+                //   at PathMap.Program.Main(String[] args) in .\Program.cs:line 20
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        static void M([CallerFilePath] string path = null) => Console.WriteLine(path);
+        static void Throw() => throw new Exception();
+    }
+}
