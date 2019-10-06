b52642ebcb4ea574ed8f92800c504d51d56e6550
Nobuyuki Iwanaga <ufcpp@live.jp>
2019-10-06 20:45:16 +0900

    Async Stream sample (gRPC)

---
+++ Demo/2019/NetCoreGrpc/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService1;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static GrpcService1.Sample;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("press enter to start");
+            Console.ReadLine();
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            await Sample(channel);
+
+            Console.WriteLine("press enter to exit");
+            Console.ReadLine();
+        }
+
+        private static async Task Sample(GrpcChannel channel)
+        {
+            var client = new SampleClient(channel);
+            var call = client.GetValues();
+
+            async Task sender()
+            {
+                for (int i = 1; i < 32; i++)
+                {
+                    await call.RequestStream.WriteAsync(new SampleRequest { Bits = i, Length = i });
+                    await Task.Delay(500);
+                }
+                await call.RequestStream.CompleteAsync();
+            }
+
+            async IAsyncEnumerable<int> receiver()
+            {
+                await foreach (var res in call.ResponseStream.ReadAllAsync())
+                {
+                    foreach (var value in res.Values)
+                    {
+                        yield return value;
+                    }
+                }
+            }
+
+            _ = sender();
+
+            await foreach (var value in receiver())
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
---
+++ Demo/2019/NetCoreGrpc/GrpcService1/Services/SampleService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace GrpcService1
+{
+    public class SampleService : Sample.SampleBase
+    {
+        public override async Task GetValues(IAsyncStreamReader<SampleRequest> requestStream, IServerStreamWriter<SampleResponse> responseStream, ServerCallContext context)
+        {
+            var rand = new Random();
+
+            await foreach (var req in requestStream.ReadAllAsync())
+            {
+                var mask = (1 << req.Bits) - 1;
+                var len = req.Length;
+
+                var res = new SampleResponse();
+
+                for (int i = 0; i < len; i++)
+                {
+                    res.Values.Add(rand.Next() & mask);
+                }
+
+                await responseStream.WriteAsync(res);
+            }
+        }
+    }
+}
