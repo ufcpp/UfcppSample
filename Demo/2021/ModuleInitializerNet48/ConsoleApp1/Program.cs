5da383acb11bad234cebe5cb6a03c1c167b1f6e2
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-02-08 19:09:35 +0900

    module initializers in .NET earlier than 5.0

---
+++ Demo/2021/ModuleInitializerNet48/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Main");
+
+class C
+{
+    [ModuleInitializer]
+    public static void M() => Console.WriteLine("ModuleInitializer");
+}
+
+namespace System.Runtime.CompilerServices
+{
+    class ModuleInitializerAttribute : Attribute { }
+}
