198556e887fcda4d6271a7aab2ce01aff39fb0cf
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-03-15 17:29:52 +0900

    keycap 用テーブル

--- Demo/2021/EmojiData/RgiSequenceFinder.TableGenerator/Program.cs
+++ Demo/2021/EmojiData/RgiSequenceFinder.TableGenerator/Program.cs
@@ -1,6 +1,8 @@
 ﻿using RgiSequenceFinder;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 var data = Data.RgiEmojiSequenceList;
 
@@ -38,7 +40,31 @@ for (int i = 0; i < data.Length; i++)
     }
 }
 
-Console.WriteLine(others.Count);
-Console.WriteLine(keycaps.Count);
-Console.WriteLine(riFlags.Count);
-Console.WriteLine(tagFlags.Count);
+using var writer = new StreamWriter("RgiTable.cs", false, Encoding.UTF8);
+
+writer.Write(@"namespace RgiSequenceFinder
+{
+    class RgiTable
+    {
+        private static int FindKeycap(Keycap key) => key switch
+        {
+");
+
+foreach (var (key, index) in keycaps)
+{
+    writer.Write("            (byte)'");
+    writer.Write((char)key.Value);
+    writer.Write("' => ");
+    writer.Write(index);
+    writer.Write(@",
+");
+}
+//Console.WriteLine(riFlags.Count);
+//Console.WriteLine(tagFlags.Count);
+//Console.WriteLine(others.Count);
+
+writer.Write(@"            _ => -1,
+        };
+    }
+}
+");
