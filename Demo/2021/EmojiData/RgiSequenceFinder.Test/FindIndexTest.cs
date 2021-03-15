e044359dde6ef93f1a191f8d0f4ac68847225e65
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-03-15 18:55:06 +0900

    単純な検索ロジック実装
    todo コメントに書いた通り、ZWJ, FE0F, 1F3FB～1F3FF への特殊対応をこの後実装したい。

---
+++ Demo/2021/EmojiData/RgiSequenceFinder.Test/FindIndexTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace RgiSequenceFinder.Test
+{
+    public class FindIndexTest
+    {
+        [Fact]
+        public void Rgi絵文字シーケンス自体のインデックスは必ず見つかる()
+        {
+            var data = Data.RgiEmojiSequenceList;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                var s = data[i];
+                var (len, index) = RgiTable.Find(s);
+
+                Assert.Equal(s.Length, len);
+                Assert.Equal(i, index);
+            }
+        }
+
+        [Fact]
+        public void Rgi絵文字シーケンスの前後にASCII文字挟んでみる()
+        {
+            var data = Data.RgiEmojiSequenceList;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                var s = data[i];
+                var s2 = "a" + s + "a";
+
+                var (len, index) = RgiTable.Find(s2);
+
+                Assert.Equal(1, len);
+                Assert.Equal(-1, index);
+
+                (len, index) = RgiTable.Find(s2.AsSpan(1));
+
+                Assert.Equal(s.Length, len);
+                Assert.Equal(i, index);
+
+                (len, index) = RgiTable.Find(s2.AsSpan(1 + len));
+
+                Assert.Equal(1, len);
+                Assert.Equal(-1, index);
+            }
+        }
+
+        [Fact]
+        public void 全RGI絵文字をConcat()
+        {
+            // 間に何も挟まずに Concat するバージョンも欲しいものの… 肌色セレクターが邪魔。
+            // 1F3FB～1F3FF は単体で RgiTable.Find に含まれているものの、GraphemeBreak 的には1個前の絵文字にくっついちゃう。
+
+            var data = Data.RgiEmojiSequenceList;
+            var sb = new StringBuilder();
+
+            foreach (var s in data)
+            {
+                sb.Append(s);
+                sb.Append('a');
+            }
+
+            var cat = sb.ToString().AsSpan();
+
+            var odd = false;
+            var i = 0;
+
+            while (true)
+            {
+                if (cat.Length == 0) break;
+
+                var (len, index) = RgiTable.Find(cat);
+
+                if (odd)
+                {
+                    odd = false;
+                    Assert.Equal(1, len);
+                    Assert.Equal('a', cat[0]);
+                }
+                else
+                {
+                    odd = true;
+                    Assert.True(data[i].AsSpan().SequenceEqual(cat[..len]));
+                    ++i;
+                }
+
+                cat = cat[len..];
+            }
+        }
+    }
+}
---
+++ Demo/2021/EmojiData/RgiSequenceFinder/RgiTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RgiSequenceFinder
+{
+    internal partial class RgiTable
+    {
+        /// <summary>
+        /// いったん、1絵文字1インデックス想定な構造で作成。
+        /// 実際には ZWJ シーケンスの場合、「見つからなかったら ZWJ でスプリットしてから再検索」とかやるので、
+        /// 1絵文字が複数のインデックスになる予定。
+        /// </summary>
+        public static (int charRead, int index) Find(ReadOnlySpan<char> s)
+        {
+            var emoji = GraphemeBreak.GetEmojiSequence(s);
+
+            var index = emoji.Type switch
+            {
+                EmojiSequenceType.Other => FindOther(s.Slice(0, emoji.LengthInUtf16).ToString()),
+                EmojiSequenceType.Flag => FindRegion(emoji.Region),
+                EmojiSequenceType.Tag => FindTag(emoji.Tags),
+                EmojiSequenceType.Keycap => FindKeycap(emoji.Keycap),
+                _ => -1,
+                // MoreBufferRequired の時は throw する？
+            };
+
+            //todo: -1 の時の再検索
+            // - ZWJ で分割して再検索
+            // - FE0F(異体字セレクター16)を消してみて再検索
+            // - 1F3FB～1F3FF (肌色選択)を消してみて再検索 + 肌色自体の絵
+            //
+            // テストに使えそうな絵文字:
+            // - Windows オリジナルキャラ: 🐱‍👤🐱‍🏍🐱‍💻🐱‍🐉🐱‍👓🐱‍🚀
+            // 1F431 200D の後ろにそれぞれ 1F464, 1F3CD, 1F4BB, 1F409, 1F453, 1F680
+            // - Windows は頑張ってレンダリングしてる4人家族×肌色: 👩🏻‍👩🏿‍👧🏼‍👧🏾
+            // 1F469 1F3FB 200D 1F469 1F3FF 200D 1F467 1F3FC 200D 1F467 1F3FE
+
+            return (emoji.LengthInUtf16, index);
+        }
+    }
+}
