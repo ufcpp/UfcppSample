2c7773459876032c97e45c04ce9fc22d14c85e74
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-08-15 13:51:36 +0900

    Activator.CreateInstance が引数なしコンストラクターを呼んでくれないバグ
    .NET Framework の事はもう諦めよう！
    
    where T : new() も狂うのが結構危ないけども…
    netstandard2.0 とかで引数なしコンストラクターを使うときには気を付けないと危ないかも。

---
+++ Demo/2021/Csharp10/ActivatorBug/Program.cs
@@ -0,0 +1,18 @@
+﻿// このコード、 .NET Core で実行すると全部1が表示されるけど、
+// .NET Framework で実行すると1つだけ1、2つ目以降は0が表示される。
+// net48 (一応最新の .NET Framework) ですらバグ挙動。
+
+using System;
+
+Console.WriteLine(Activator.CreateInstance<T>().Value);
+Console.WriteLine(Activator.CreateInstance<T>().Value);
+Console.WriteLine(New<T>().Value);
+Console.WriteLine(New<T>().Value);
+
+static T New<T>() where T : new() => new();
+
+struct T
+{
+    public int Value;
+    public T() => Value = 1;
+}
