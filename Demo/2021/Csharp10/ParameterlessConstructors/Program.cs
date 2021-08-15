60312cb83d28ece27d3faf15f58ce843a63d2b21
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-08-15 14:33:21 +0900

    record struct + parameterless constructor

--- Demo/2021/Csharp10/ParameterlessConstructors/Program.cs
+++ Demo/2021/Csharp10/ParameterlessConstructors/Program.cs
@@ -5,7 +5,3 @@ NewOrDefault.M();
 Zeroed.M();
 //Defaultable.M();
 GenericNew.M();
-
-// 他に書きたいデモ:
-// * record struct が入ったことで需要が高まって、C# 10.0 で再取組みされたという話
-
---
+++ Demo/2021/Csharp10/ParameterlessConstructors/RecordStruct.cs
@@ -0,0 +1,14 @@
+﻿namespace ParameterlessConstructors.RecordStruct;
+
+// record (class) で以下のような書き方できるんだし、record struct でも書けるようにしたい。
+// そのためには引数なしコンストラクターが必要だった。
+
+record struct A();
+
+readonly record struct B
+{
+    private static int _id;
+    private static int GetId() => Interlocked.Increment(ref _id);
+
+    public int Id { get; } = GetId();
+}
