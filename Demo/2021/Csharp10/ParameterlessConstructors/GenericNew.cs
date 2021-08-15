de18b3579b2594fc0725624aa8ea4722dee6e304
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-08-15 13:18:50 +0900

    where T : struct

--- Demo/2021/Csharp10/ParameterlessConstructors/GenericNew.cs
+++ Demo/2021/Csharp10/ParameterlessConstructors/GenericNew.cs
@@ -7,8 +7,8 @@ class GenericNew
     public static void M()
     {
         Console.WriteLine(New<Class>().X); // 0
-        Console.WriteLine(New<NoConstructor>().X); // 0
-        Console.WriteLine(New<HasConstructor>().X); // 1
+        Console.WriteLine(New<A>().X); // 0
+        Console.WriteLine(New<B>().X); // 1
     }
 
     // クラスの場合、何も書かないとき、暗黙的に引数なしコンストラクターが作られてる。
@@ -19,16 +19,43 @@ class GenericNew
 
     // 構造体の場合、何も書かないとき、「コンストラクターはない」という扱い。
     // new T() は default(T) (0 初期化)と同じ意味。
-    struct NoConstructor
+    struct A
     {
         public int X;
     }
 
     // C# 10.0 で引数なしコンストラクターを書けるようになって…
     // new T() と書くと引数なしコンストラクターが呼ばれる。
-    struct HasConstructor
+    struct B
     {
         public int X;
-        public HasConstructor() => X = 1;
+        public B() => X = 1;
+    }
+}
+
+class StructConstraint
+{
+    // struct 制約は暗黙的に new() 制約を含む。
+    // 昔は new T() と default(T) が同じ意味だったけど、今は違う意味になる。
+    static T New<T>() where T : struct => new(); // 引数なしコンストラクターがあればそれが呼ばれる。
+    static T Default<T>() where T : struct => default; // 常に 0 初期化。
+
+    public static void M()
+    {
+        Console.WriteLine(New<A>().X); // 0
+        Console.WriteLine(New<B>().X); // 1
+        Console.WriteLine(Default<A>().X); // 0
+        Console.WriteLine(Default<B>().X); // 0
+    }
+
+    struct A
+    {
+        public int X;
+    }
+
+    struct B
+    {
+        public int X;
+        public B() => X = 1;
     }
 }
