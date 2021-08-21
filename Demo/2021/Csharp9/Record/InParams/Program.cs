8ffd664dcf0217c82475f86482425d770c551712
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-08-21 20:31:24 +0900

    Csharp9 (後付け)

---
+++ Demo/2021/Csharp9/Record/InParams/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+var r = new Record(1, 2, 3, 4);
+
+Console.WriteLine(r.X); // 1
+
+foreach (var item in r.Y)
+{
+    Console.WriteLine(item); // 2, 3, 4
+}
+
+// レコード型のプライマリ コンストラクター引数(からのプロパティ生成)、
+// in と params は受け付けるらしい。
+public record Record(in int X, params int[] Y);
+
+// ちなみに、 ref と out はダメ。
+//public record Record2(ref int X, out int Y);
