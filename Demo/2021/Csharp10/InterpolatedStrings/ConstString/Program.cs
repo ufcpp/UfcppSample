4b3e804c2cfa141ce123b3ff7f01fd5c02498782
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-09-18 18:38:45 +0900

    const string interpolation

---
+++ Demo/2021/Csharp10/InterpolatedStrings/ConstString/Program.cs
@@ -0,0 +1,26 @@
+﻿// {} 内に文字列以外の const を指定できない理由の1つが、ToString のカルチャー依存:
+
+using System.Globalization;
+
+// 東南アジアの多くの国は . を小数点に使う。
+Thread.CurrentThread.CurrentCulture = new CultureInfo("ja-jp");
+Console.WriteLine(1.234);
+
+// 大陸ヨーロッパの多くの国は , を小数点に使う。
+Thread.CurrentThread.CurrentCulture = new CultureInfo("fr-fr");
+Console.WriteLine(1.234);
+
+class Valid
+{
+    const string A = "Abc";
+    const string B = "Xyz";
+    public const string C = $"{nameof(A)}: {A}, {nameof(B)}: {B}"; // "A: Abc, B: Xyz"
+}
+
+#if Error
+class Invalid
+{
+    const int A = 1;
+    const string C = $"{A}"; // A が文字列じゃないので $"" の結果を const にできない。}
+}
+#endif
