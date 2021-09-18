0058ce9c8cf3631fbaaa0381915a17f0632abe35
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-09-18 14:01:31 +0900

    オプション引数と構造体の new()

---
+++ Demo/2021/Csharp10/ParameterlessConstructors/DefaultParameter/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(new Ctor());
+
+#if Error
+void m(
+    NoCtor n1 = new(),
+    NoCtor n2 = default,
+    Ctor c1 = new(), // この行だけコンパイル エラー
+    Ctor c2 = default
+    )
+{ }
+#endif
+
+struct NoCtor { }
+struct Ctor { public Ctor() { } }
