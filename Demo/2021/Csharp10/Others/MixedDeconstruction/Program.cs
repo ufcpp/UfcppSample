b1bd6d7eb8d6230b6ebfc17d2662a91c80bec99c
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-09-20 16:08:32 +0900

    MixedDeconstruction

---
+++ Demo/2021/Csharp10/Others/MixedDeconstruction/Program.cs
@@ -0,0 +1,14 @@
+﻿int x;
+(x, var u) = (1, 2);
+
+Console.WriteLine(x);
+Console.WriteLine(u);
+
+#if ERROR
+void m()
+{
+    // 式の途中に分解宣言 (var 付きの宣言) が来るのは C# 10.0 でもダメ。
+    int x, y;
+    (x, var u) = (var v, y) = (1, 2);
+}
+#endif
