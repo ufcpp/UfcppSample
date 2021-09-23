d3b187067c9265b54935b9517f7ab9e36078fc9e
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-09-23 21:45:36 +0900

    オーバーロード解決

---
+++ Demo/2021/Csharp10/InterpolatedStrings/OverloadResolution/C1.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OverloadResolution;
+
+internal class C1
+{
+    public static void Caller()
+    {
+        // ハンドラー型最優先。
+        M($"{1}"); // handler
+
+        // ただの文字列の場合は string に行く。
+        M("abc"); // string
+
+        // ちょっと混乱しそうなのが、const になる場合に限り、 $ がついてても string 行き。
+        M($""); // string
+        M($"abc {"abc"} abc"); // string
+
+        // もちろん、キャストしてしまえば任意に呼び分け可能。
+        M($"{1}"); // handler
+        M((string)$"{1}"); // string
+        M((IFormattable)$"{1}"); // formattable
+    }
+
+    // この状態だとハンドラー型優先。
+    // DefaultInterpolatedStringHandler だけ特別とかはなく、ハンドラー パターンを満たす型は一律この優先順位。
+    public static void M(DefaultInterpolatedStringHandler _) => Console.WriteLine("handler");
+    public static void M(string _) => Console.WriteLine("string");
+    public static void M(IFormattable _) => Console.WriteLine("formattable");
+}
---
+++ Demo/2021/Csharp10/InterpolatedStrings/OverloadResolution/C2.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OverloadResolution;
+
+internal class C2
+{
+    public static void Caller()
+    {
+        // 優先度は付かないので不明瞭エラーを起こす。
+#if ERROR
+        M($"");
+#endif
+
+        // 明示的にキャストすれば呼び分け可能。
+        M((Handler1)$"");
+        M((Handler2)$"");
+    }
+
+    public static void M(Handler1 _) => Console.WriteLine("Handler1");
+    public static void M(Handler2 _) => Console.WriteLine("Handler2");
+}
+
+[InterpolatedStringHandler]
+public struct Handler1
+{
+    public Handler1(int literalLength, int formattedCount) { }
+    public void AppendLiteral(string s) { }
+    public void AppendFormatted<T>(T x) { }
+}
+
+[InterpolatedStringHandler]
+public struct Handler2
+{
+    public Handler2(int literalLength, int formattedCount) { }
+    public void AppendLiteral(string s) { }
+    public void AppendFormatted<T>(T x) { }
+}
