0e6bc2e154256052ab13fedc8644d9b746445443
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-09-23 19:11:34 +0900

    AppendFormattedOverload

---
+++ Demo/2021/Csharp10/InterpolatedStrings/AppendFormattedOverload/Program.cs
@@ -0,0 +1,11 @@
+﻿DummyHandler h = $"{123}, {"abc"}, {stackalloc char[1]}";
+
+[System.Runtime.CompilerServices.InterpolatedStringHandler]
+public struct DummyHandler
+{
+    public DummyHandler(int literalLength, int formattedCount) { }
+    public void AppendLiteral(string s) => Console.WriteLine("(literal)");
+    public void AppendFormatted<T>(T x) => Console.WriteLine("ジェネリック版");
+    public void AppendFormatted(string x) => Console.WriteLine("string 版");
+    public void AppendFormatted(ReadOnlySpan<char> x) => Console.WriteLine("ReadOnlySpan 版");
+}
