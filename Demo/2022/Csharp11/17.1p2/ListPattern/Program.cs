0384b8c049ace046b04782bc6e1aa56a7d14980b
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-04-07 21:40:31 +0900

    List Patterns

---
+++ Demo/2022/Csharp11/17.1p2/ListPattern/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+var x = new[] { 1, 2, 3, 4, 5 };
+
+if (x.AsSpan() is [1, .. var y, 5]) // これで、1開始、5終端にマッチしたとき、真ん中の要素を取る。
+{
+    foreach (var item in y) // 2, 3, 4
+    {
+        Console.WriteLine(item);
+    }
+}
+
+// こんなのも有効な C# コードだったりする。
+// 特に意味はない。 new int[0] is [] と一緒。結局、 Length == 0 と一緒。
+if (new int[] { }[..][..][..] is [.. [.. []]])
+{
+}
+
+// こういう使い方なら実用的かなぁという例。
+// UTF-8 なシーケンスから BOM の除去。
+var utf8 = Encoding.UTF8.GetBytes("何か").AsSpan();
+
+if (utf8 is [0xEF, 0xBB, 0xBF, .. var noBom])
+{
+    utf8 = noBom;
+}
+
+foreach (var c in utf8)
+{
+    Console.WriteLine(c);
+}
+
+// まあ、よくあるのは関数型言語でよく見る head-tail。
+m(new byte[] { 1, 2, 3, 4 });
+
+void m(Span<byte> span)
+{
+    if (span is [var head, .. var tail])
+    {
+        m(tail);
+        Console.WriteLine(head);
+    }
+}
