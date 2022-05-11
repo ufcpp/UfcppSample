4059e92f96c928dfbf392de580f9a05f7acfa5ae
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-05-11 23:42:00 +0900

    checked 演算子オーバーロード

---
+++ Demo/2022/Csharp11/17.3p1/CheckedOperator/Program.cs
@@ -0,0 +1,39 @@
+﻿// 通常(オプション指定がないとき、unchecked 扱い)、整数型の算術演算はオーバーフローチェックしない。
+var x = new Int4Bit(3);
+++x;
+Console.WriteLine(x.Value);
+
+// 同じ処理でも checked ブロックで囲うと…
+try
+{
+    var y = new Int4Bit(3);
+
+    checked
+    {
+        ++y;
+    }
+}
+catch
+{
+    // ここに来るはず。
+    Console.WriteLine("overflow");
+}
+
+//## checked operator の定義
+// こんな感じの特殊用途な整数型を作ったとして
+struct Int4Bit
+{
+    private readonly byte _value;
+    public Int4Bit(int value) => _value = (byte)(value & 3);
+    public int Value => _value;
+
+    // デモ用に1個だけ演算子を実装。とりあえず簡単な ++ を。
+    public static Int4Bit operator ++(Int4Bit x) => new(x._value + 1);
+
+    // この型、このままだと int とかの組み込み型と違って、「オーバーフローした時の挙動」を選べない。
+    // 組み込み型に準ずるユーザー定義型を作りたいのにできないことがあった。
+
+    // generic math も導入されることだし、この「できないことがある」問題解消の動きが出てきた。
+    // operator checked って文法で、 unchecked/checkd で別実装を持てる。
+    public static Int4Bit operator checked ++(Int4Bit x) => x._value < 3 ? new(x._value + 1) : throw new Exception();
+}
