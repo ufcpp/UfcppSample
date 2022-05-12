baa84b45b518a920e3f2b2f7994ef732e84e38e6
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-05-12 16:26:18 +0900

    Pattern matching on ReadOnlySpan<char>

---
+++ Demo/2022/Csharp11/17.3p1/PatternROS/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine(m("true"));
+Console.WriteLine(m("false"));
+Console.WriteLine(m("other"));
+
+// ReadOnlySpan<char> に対して is とか switch で文字列リテラルとの比較ができるようになった。
+static bool? m(ReadOnlySpan<char> s) => s switch
+{
+    "true" => true,
+    "false" => false,
+    _ => null,
+};
+
+#if false
+// ちなみに、UTF-8 リテラルを使って ReadOnlySpan<byte> にマッチさせるのは無理。
+// 「""u8 は const じゃないからパターンに使えない」みたいな理由。
+// (C# 11 正式リリースでもこれはできないままな可能性大。)
+
+static bool? error(ReadOnlySpan<byte> s) => s switch
+{
+    "true"u8 => true,
+    "false"u8 => false,
+    _ => null,
+};
+#endif
