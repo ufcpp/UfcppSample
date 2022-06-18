46be59f37afd5f99f75eec7b070ad8a4191c09f0
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-06-18 18:39:59 +0900

    required メンバー

---
+++ Demo/2022/Csharp11/17.3p2/RequiredMembers/MembersNotNull.cs
@@ -0,0 +1,71 @@
+﻿#pragma warning disable CS8618
+
+//### 非 null 参照型フィールド/プロパティ
+class MembersNotNull
+{
+    // C# 8.0 で null 共用参照型が入って以来、この X, Y に出てくる警告を真っ当に消す手段がなかった。
+    // 非 null な参照型のメンバーはコンストラクター内で初期化されないといけない。
+    // (このソースコードでは pragma で抑止してる。真っ当に消せるはずの警告も一緒に消えちゃったりするんであんまりよくない。)
+    //
+    // もちろん、コンストラクターを用意すれば解消はするものの…
+    // C# 9.0 で init の意味がちょっと薄れる。
+    public string X { get; init; }
+    public string Y { get; init; }
+}
+
+//### positional 初期化
+
+// 「コンストラクターを用意すれば解決とは言ったものの…
+// record がない頃はコンストラクターを書くのも結構面倒。
+
+class InitWithConstructor
+{
+    // x, y の文字をそれぞれ4回書く必要あり。
+    public string X { get; init; }
+    public string Y { get; init; }
+    public InitWithConstructor(string x, string y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    // さらに言うと、後からプロパティを足そうとすると…
+    public string Z { get; init; }
+
+    // コンストラクターももう1回書き直し。
+    public InitWithConstructor(string x, string y, string z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
+
+// record にはプライマリ コンストラクターっていう仕様があって、
+// 以下のようなコードから上記のようなプロパティとコンストラクターを生成してくれる。
+// これで多少面倒は減ったものの…
+record PositionalBase(string X, string Y);
+
+// 派生クラスを作ったときに X, Y をどこかから伝搬しないといけない。
+// だいたいは以下のように X, Y を再度書く必要あり。
+record PositionalDerived(string X, string Y, string Z) : PositionalBase(X, Y);
+
+//### nominal 初期化
+//
+// コンストラクター引数(順序に意味があるので「positional (位置による) 初期化」という)だから追加・拡張が面倒なので…
+// プロパティ初期化(名前に意味があるので「nominal (名前による)初期化」という)が欲しくなったり。
+
+record NominalBase
+{
+    public string X { get; init; }
+    public string Y { get; init; }
+}
+
+record NominalDerived : NominalBase
+{
+    // X, Y の伝搬不要！
+    public string Z { get; init; }
+}
+
+// そして冒頭の問題に戻る。
+// NominalBase に出てる「非 null 参照型プロパティはちゃんと初期化しろ」警告が消せない。 
--- Demo/2022/Csharp11/17.3p2/RequiredMembers/Program.cs
+++ Demo/2022/Csharp11/17.3p2/RequiredMembers/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//## Required メンバー
+//
+// C# 8.0 で null 許容参照型が入って以来ずっとある課題。
+// 非 null な参照型フィールド/プロパティがあると、コンストラクターが必須になっちゃう。
+// コンストラクターを用意するのが結構めんどくさい場面も多々あってしんどかった。
+// MembersNotNull.cs を参照。
+
+using System.Diagnostics.CodeAnalysis;
+
+//### required 修飾子
+//
+// そこで C# 11 で導入されるのが required。
+// プロパティ(とか、public フィールド)に付けておくと、オブジェクト初期化子で初期化することを義務付けられるようになった。
+
+var a = new A
+{
+    // ここの2行、どちらか片方でも削るとコンパイル エラーになる。
+    X = "",
+    Y = 1,
+};
+
+Console.WriteLine(a);
+
+record A
+{
+    // required を付けると初期化子での初期化が必須になる。
+    public required string X { get; init; }
+    public required int Y { get; init; }
+
+    public A() { }
+
+    // ちなみに、「コンストラクターでちゃんと初期化してるから、初期化子で改めて値を渡す必要はないよ」みたいな実装をしたい場合、
+    // SetsRequiredMembers 属性を付ける。
+    // こっちなら var a = new A("", 1); とか書ける。
+    [SetsRequiredMembers]
+    public A(string x, int y)
+    {
+        X = x;
+        Y = y;
+
+        // ただし注意が2点:
+        // * 「X だけは初期化したけど、Y は初期化子で渡してくれ」みたいな部分指定はできない
+        // * このコンストラクター内で Y に何も代入しなくても特に警告は出ない。実装者任せ
+    }
+
+    // 未対応のバージョンのコンパイラーで new A(); だけ呼ばれると困るので、
+    // A() の方には「対応していなければエラーになる属性」みたいなのを付けるみたい。
+    // (今のところ Obsolete 属性を使ってやってる。)
+}
+
+// ちなみに、virtual にしたりできる。
+// (abstract も可。)
+class Base
+{
+    public virtual required string X { get; set; }
+}
+
+class Derived : Base
+{
+    // 派生側で required を外すのはダメ。コンパイル エラーに。
+    //public override string X { get; init; }
+
+    // これなら OK。
+    public override required string X { get; set; }
+}
