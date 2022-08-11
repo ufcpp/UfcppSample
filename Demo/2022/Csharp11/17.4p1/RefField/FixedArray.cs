94bc0e923305403eb9205ba91d5102b6ba73bacf
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-08-11 15:57:29 +0900

    ref fields

---
+++ Demo/2022/Csharp11/17.4p1/RefField/FixedArray.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable IDE0044, IDE0051, CS0169
+
+using System.Runtime.InteropServices;
+
+namespace RefField;
+
+public ref struct FixedArray4<T>
+{
+    private T _x0, _x1, _x2, _x3;
+
+#if NET7_PREVIEW7
+    // ほんとは UnscopedRef 属性ってのがあって、それに対応した暁には解決するものの…
+    // 現状、この行をコンパイルする手段がない。
+    public ref T this[int index] => ref Unsafe.Add(ref _x0, index);
+#endif
+
+    // MemoryMarshal.CreateSpan は互換性のため / Unsafe な手段を取りたいときのために、
+    // ref 引数が scoped ref になってる(.NET 7 から)。
+    // なので、このコードはコンパイルできる。
+    // ただし… これを TargetFramework net6.0 に変えたらコンパイルできなくなる(scoped 修飾子がないので)。
+    public ref T this[int index] => ref MemoryMarshal.CreateSpan(ref _x0, 4)[index];
+}
---
+++ Demo/2022/Csharp11/17.4p1/RefField/Program.cs
@@ -0,0 +1,43 @@
+﻿#pragma warning disable CS8321
+
+Console.WriteLine("Hello, World!");
+
+static A m1()
+{
+    var a = new A();
+    return a;
+}
+
+static A m2(ref int x)
+{
+    var a = new A() { X = ref x };
+    return a;
+}
+
+#if ERROR
+static A m3()
+{
+    var x = 1;
+    var a = new A();
+    a.X = ref x; // エラー: local x を scoped じゃない a に代入。
+    return a; // その代わり、return しても OK。
+}
+#endif
+
+static void m4()
+{
+    var x = 1;
+    scoped var a = new A();
+    a.X = ref x; // これは OK。
+}
+
+#if ERROR
+static A m5()
+{
+    var x = 1;
+    scoped var a = new A { X = ref x };
+    return a; // エラー: scoped a を return。
+}
+#endif
+
+ref struct A { public ref int X; }
