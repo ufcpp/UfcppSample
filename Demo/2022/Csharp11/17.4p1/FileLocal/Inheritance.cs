fb3d6e6aba33dc863e776394945f377b6ca510d8
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-08-13 23:49:04 +0900

    file-local + 派生

---
+++ Demo/2022/Csharp11/17.4p1/FileLocal/Inheritance.cs
@@ -0,0 +1,22 @@
+﻿namespace FileLocal;
+
+// file-local なクラスで public なクラスを継承。
+file class FileDerived : Base
+{
+}
+
+public class Base
+{
+    // これはダメ。
+    // file-local な型を public な引数・戻り値には書けない。
+    //public static FileDerived Create() => new X();
+
+    // これは書けるので、大体これでやる。
+    public static Base Create() => new FileDerived();
+}
+
+#if ERROR
+// 逆はダメ。
+file class FileBase { }
+class Derived : FileBase { } // CS9053 エラー。
+#endif
