a898acf91f74ebb436523621adc1715887d8f4e3
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-08-13 23:44:53 +0900

    file-local interface

---
+++ Demo/2022/Csharp11/17.4p1/FileLocal/Interface.cs
@@ -0,0 +1,16 @@
+﻿// file-local なインターフェイスも OK だし、
+// それを public な型で実装するのも OK。
+
+public class CX : IX // OK
+{
+    // file 修飾子は top-level の型にしか付けれないみたいで、
+    // file void M() { } とかは書けない。
+
+    // でも、 file-local インターフェイス で明示的実装しておけば実質 file-local メソッドに。
+    void IX.M() { }
+}
+
+file interface IX
+{
+    void M(); // これを明示的実装すれば file 内限定メソッド作れる。
+}
