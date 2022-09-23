0a400cb604c41d1a4d31aeb7dc68169aec455682
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-09-23 23:51:58 +0900

    whitespaces

---
+++ Demo/2022/Csharp11/17.2p1/RawStringLiteral/Whitespaces.cs
@@ -0,0 +1,25 @@
+﻿// see https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure
+
+namespace RawStringLiteral;
+
+internal class Whitespaces
+{
+    // C# is not whitespace sensitive.
+	               public const string S = """
+	               abc
+	               """;
+
+    public static void M()
+    {
+        // Whitespace
+        //     : [\p{ Zs}]  // any character with Unicode class Zs
+        //     | '\u0009'  // horizontal tab
+        //     | '\u000B'  // vertical tab
+        //     | '\u000C'  // form feed
+        //     ;
+        foreach (var c in "	                ")
+        {
+            Console.WriteLine($"{(int)c:X}");
+        }
+    }
+}
