2187f31bc9868c7b468e75c6fd389e402e6fea93
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-09-24 11:32:56 +0900

    whitespace

---
+++ Demo/2022/Whitespace/Program.cs
@@ -0,0 +1,8 @@
+﻿//↓ U+0001～001F except for tab (09), CR (0D), LF (0A)
+//
+// Visual Studio displays these control characters as symbols.
+
+// U+000B, U+000C, and U+001A are treated as whitespace in C#, thus, the folloing is a valid C# code.
+var a = 1;
+if (a > 0)
+Console.WriteLine(a);
