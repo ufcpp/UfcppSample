bc2a2f61e71c30c8aa06a488e0004c146664dffa
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-12-20 22:14:51 +0900

    ScribanSourceGenerator 利用例

---
+++ Demo/2022/NoMoreT4/FileGenerator/Y.cs
@@ -0,0 +1,12 @@
+﻿namespace FileGenerator;
+
+[ScribanSourceGeneretor.ClassMember("""
+    {{
+    for $t in ["bool","byte","int","double"]
+    ~}}
+        public static bool TryParse(this string s, out {{$t}} x) => {{$t}}.TryParse(s, out x);
+    {{ end }}
+    """)]
+internal static partial class Extensions
+{
+}
